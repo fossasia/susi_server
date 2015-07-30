68e969ae5473d9e1bcc2842e21ea11c9044aca90
Michael Peter Christen <mc@yacy.net>
2015-07-30 15:05:47 +0200

    splitted the account servlet into two parts: one which contains sensitive user information (account.json) and must be protected against non-authentified users, and a 'user.json' servlet which provides public accessible information.

--- src/org/loklak/LoklakServer.java
+++ src/org/loklak/LoklakServer.java
@@ -63,6 +63,7 @@ import org.loklak.api.server.HelloServlet;
 import org.loklak.api.server.PeersServlet;
 import org.loklak.api.server.ProxyServlet;
 import org.loklak.api.server.PushServlet;
+import org.loklak.api.server.UserServlet;
 import org.loklak.api.server.push.GeoJsonPushServlet;
 import org.loklak.api.server.SearchServlet;
 import org.loklak.api.server.SettingsServlet;
@@ -206,6 +207,7 @@ public class LoklakServer {
         servletHandler.addServlet(SearchServlet.class, "/api/search.json"); // both have same servlet class
         servletHandler.addServlet(SuggestServlet.class, "/api/suggest.json");
         servletHandler.addServlet(AccountServlet.class, "/api/account.json");
+        servletHandler.addServlet(UserServlet.class, "/api/user.json");
         servletHandler.addServlet(CampaignServlet.class, "/api/campaign.json");
         servletHandler.addServlet(SettingsServlet.class, "/api/settings.json");
         servletHandler.addServlet(GeocodeServlet.class, "/api/geocode.json");
--- src/org/loklak/api/server/UserServlet.java
+++ src/org/loklak/api/server/UserServlet.java
@@ -1,5 +1,5 @@
 /**
- *  AccountServlet
+ *  UserServlet
  *  Copyright 27.05.2015 by Michael Peter Christen, @0rb1t3r
  *
  *  This library is free software; you can redistribute it and/or
@@ -20,9 +20,7 @@
 package org.loklak.api.server;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -31,7 +29,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.loklak.data.AccountEntry;
 import org.loklak.data.DAO;
 import org.loklak.data.UserEntry;
 import org.loklak.harvester.TwitterAPI;
@@ -40,7 +37,7 @@ import twitter4j.TwitterException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class AccountServlet extends HttpServlet {
+public class UserServlet extends HttpServlet {
    
     private static final long serialVersionUID = 8578478303032749879L;
 
@@ -49,61 +46,28 @@ public class AccountServlet extends HttpServlet {
         doGet(request, response);
     }
     
-    @SuppressWarnings("unchecked")
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RemoteAccess.Post post = RemoteAccess.evaluate(request);
      
         // manage DoS
         if (post.isDoS_blackout()) {response.sendError(503, "your request frequency is too high"); return;}
-        if (!post.isLocalhostAccess()) {response.sendError(503, "access only allowed from localhost, your request comes from " + post.getClientHost()); return;} // danger! do not remove this!
         
         // parameters
         String callback = post.get("callback", "");
         boolean jsonp = callback != null && callback.length() > 0;
         boolean minified = post.get("minified", false);
-        boolean update = "update".equals(post.get("action", ""));
         String screen_name = post.get("screen_name", "");
+        String followers = post.get("followers", "0");
+        String following = post.get("following", "0");
+        int maxFollowers = Integer.parseInt(followers);
+        int maxFollowing = Integer.parseInt(following);
         
-        String data = post.get("data", "");
-        if (update) {
-            if  (data == null || data.length() == 0) {
-                response.sendError(400, "your request does not contain a data object.");
-                return;
-             }
-        
-            // parse the json data
-            try {
-                Map<String, Object> map = DAO.jsonMapper.readValue(data, DAO.jsonTypeRef);
-                Object accounts_obj = map.get("accounts");
-                List<Map<String, Object>> accounts;
-                if (accounts_obj instanceof List<?>) {
-                    accounts = (List<Map<String, Object>>) accounts_obj;
-                } else {
-                    accounts = new ArrayList<Map<String, Object>>(1);
-                    accounts.add(map);
-                }
-                for (Map<String, Object> account: accounts) {
-                    if (account == null) continue;
-                    try {
-                        AccountEntry a = new AccountEntry(account);
-                        DAO.writeAccount(a, true);
-                    } catch (IOException e) {
-                        response.sendError(400, "submitted data is not well-formed: " + e.getMessage());
-                        e.printStackTrace();
-                        return;
-                    }
-                }
-                if (accounts.size() == 1) {
-                    screen_name = (String) accounts.iterator().next().get("screen_name");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
         UserEntry userEntry = DAO.searchLocalUserByScreenName(screen_name);
-        AccountEntry accountEntry = DAO.searchLocalAccount(screen_name);
+        Map<String, Object> twitterUserEntry = null;
+        try {twitterUserEntry = TwitterAPI.getUser(screen_name);} catch (TwitterException e) {}
+        Map<String, Object> topology = null;
+        try {topology = TwitterAPI.getNetwork(screen_name, maxFollowers, maxFollowing);} catch (TwitterException e) {}
         
         post.setResponse(response, "application/javascript");
         
@@ -114,14 +78,8 @@ public class AccountServlet extends HttpServlet {
         metadata.put("client", post.getClientHost());
         m.put("search_metadata", metadata);
 
-        // create a list of accounts. Why a list? Because the same user may have accounts for several services.
-        List<Object> accounts = new ArrayList<>();
-        if (accountEntry == null) {
-            if (userEntry != null) accounts.add(AccountEntry.toEmptyAccount(userEntry));
-        } else {
-            accounts.add(accountEntry.toMap(userEntry));
-        }
-        m.put("accounts", accounts);
+        if (twitterUserEntry != null) m.put("user", twitterUserEntry);
+        if (topology != null) m.put("topology", topology);
         
         // write json
         ServletOutputStream sos = response.getOutputStream();
