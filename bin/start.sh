cf83ee74b83cd3a1b62c2abe1b56f82e67ff6455
Michael Peter Christen <mc@yacy.net>
2015-08-06 21:40:39 +0200

    reverted XmX setting change. I know that this fixed memory allocation is bad but we currently don't have a dynamic design for this. And 4Gb is needed in production. Please add an option to read the Xmx from settings if possible.

--- bin/start.sh
+++ bin/start.sh
@@ -5,6 +5,6 @@ CLASSPATH=""
 for N in lib/*.jar; do CLASSPATH="$CLASSPATH$N:"; done
 CLASSPATH=".:./classes/:$CLASSPATH"
 echo "starting loklak"
-java -Xmx1G -Xms1G -server -XX:+AggressiveOpts -XX:NewSize=512M -classpath $CLASSPATH org.loklak.LoklakServer >> data/loklak.log 2>&1 & echo $! > data/loklak.pid &
+java -Xmx4G -Xms1G -server -XX:+AggressiveOpts -XX:NewSize=512M -classpath $CLASSPATH org.loklak.LoklakServer >> data/loklak.log 2>&1 & echo $! > data/loklak.pid &
 
 echo "loklak server started at port 9000, open your browser at http://localhost:9000"
