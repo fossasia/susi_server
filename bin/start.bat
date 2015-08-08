912df03b06098c189f47d0acf3be85ab5aed9c0f
aneeshd16 <aneeshdevasthale@gmail.com>
2015-08-08 12:23:18 +0530

    Modified .bat file with new startup parameters

--- bin/start.bat
+++ bin/start.bat
@@ -1,3 +1,3 @@
 cd ..
-START java -Xmx2G -Xms2G -server -XX:+AggressiveOpts -XX:NewSize=512M -cp "classes;lib/*" org.loklak.LoklakServer >> data/loklak.log 2>&1 & echo $! > data/loklak.pid
+START java -Xmx4G -Xms1G -server -XX:+AggressiveOpts -XX:NewSize=512M -cp "classes;lib/*" org.loklak.LoklakServer >> data/loklak.log 2>&1 & echo $! > data/loklak.pid
 echo "loklak server started at port 9100, open your browser at http://localhost:9100"
