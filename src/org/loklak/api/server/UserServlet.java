2968c3a2a689095b4c96e3cf7183fd0857e82d33
Michael Peter Christen <mc@yacy.net>
2015-08-08 02:28:29 +0200

    fix for problem addressed in https://github.com/loklak/loklak_webclient/issues/413#issuecomment-127078292

--- src/org/loklak/api/server/UserServlet.java
+++ src/org/loklak/api/server/UserServlet.java
@@ -29,8 +29,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.loklak.data.DAO;
-import org.loklak.data.UserEntry;
 import org.loklak.harvester.TwitterAPI;
 
 import twitter4j.TwitterException;
