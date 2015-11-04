81ae29626c066d78d91f77401082b97f2f1e0f8a
Michael Peter Christen <mc@yacy.net>
2015-11-04 21:39:23 +0100

    feeding of pre-defined queries into the automatic query harvester. To feed the harvester, files with queries in lines of a text file must be placed into the data/queries folder. This will be read once upon startup and imported into the local query database.

--- src/org/loklak/data/DAO.java
+++ src/org/loklak/data/DAO.java
@@ -19,9 +19,12 @@
 
 package org.loklak.data;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.AbstractMap;
 import java.util.ArrayList;
@@ -281,6 +284,41 @@ public class DAO {
             // start the classifier
             Classifier.init(10000, 1000);
             log("classifier initialized!");
+            
+            // initialize query harvesting
+            if (getConfig("retrieval.queries.enabled", false)) {
+                File harvestingPath = new File(datadir, "queries");
+                if (!harvestingPath.exists()) harvestingPath.mkdirs();
+                String[] list = harvestingPath.list();
+                for (String queryfile: list) {
+                    if (queryfile.startsWith(".") || queryfile.endsWith("~")) continue;
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(harvestingPath, queryfile))));
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            line = line.trim();
+                            if (line.length() == 0) continue;
+                            if (line.charAt(0) <= '9') {
+                                // truncate statistic
+                                int p = line.indexOf(' ');
+                                if (p < 0) continue;
+                                line = line.substring(p + 1).trim();
+                            }
+                            // write line into query database
+                            if (!existQuery(line)) {
+                                try {
+                                    queries.writeEntry(line, SourceType.TWITTER.name(), new QueryEntry(line, 0, 60000, SourceType.TWITTER, false));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         } catch (Throwable e) {
             e.printStackTrace();
         }
