acb9032469a2d2d25d89819abffa548968860d0b
Jasper Yu <007seadog@gmail.com>
2015-12-09 07:14:42 +0800

    Added search tool in Perl

---
+++ bin/search.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use LWP::Simple;                # From CPAN
+use JSON qw( decode_json );     # From CPAN
+use strict;                     # Good practice
+use warnings;                   # Good practice
+
+my $num_args = $#ARGV + 1;
+if ($num_args != 1) {
+	print "\nUsage: search.pl string\n";
+	exit;
+}
+
+my $url = "http://loklak.org/api/search.json?q=$ARGV[0]";
+my $json = get( $url );
+die "Could not get $url!" unless defined $json;
+
+my $decoded_json = decode_json( $json );
+
+foreach my $tweets(@{$decoded_json->{'statuses'}}){
+	my %tweets_hash = ();
+	$tweets_hash{text} = "$tweets->{'text'}";
+	while (my($k, $v) = each (%tweets_hash)){
+		print "$v\n";
+	}
+}
