e09261045a0b62a6766d0627b17dd4265aad0d42
Jasper Yu <007seadog@gmail.com>
2015-12-08 16:58:18 +0800

    Added deps info

--- bin/search.sh
+++ bin/search.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# You should install jq and curl
+# if you are using OSX, then you can use brew install curl jq
+# And if you are using Ubuntu, then use apt-get install curl jq
 curl "http://loklak.org/api/search.json?q=$1" | jq '.statuses[].text'
