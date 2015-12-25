951c0330eebfdf898bf7516e6aff77537c262c5d
Michael Peter Christen <mc@yacy.net>
2015-12-25 12:20:49 +0100

    set a generic cluster name to prevent that two loklak instances join each other

--- src/org/loklak/data/DAO.java
+++ src/org/loklak/data/DAO.java
@@ -236,7 +236,7 @@ public class DAO {
             }
             
             // start elasticsearch
-            elasticsearch_node = NodeBuilder.nodeBuilder().settings(settings).node();
+            elasticsearch_node = NodeBuilder.nodeBuilder().client(false).clusterName("loklak_" + System.currentTimeMillis()).settings(settings).node();
             elasticsearch_client = elasticsearch_node.client(); // TransportClient.builder().settings(settings).build();
 
             Path index_dir = dataPath.resolve("index");
