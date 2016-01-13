bdb172d834479071226b4d3c5ec2345c7f5af9da
MCHRISTE <MCHRISTE@S0105112.ocn.ottogroup.com>
2016-01-13 18:51:36 +0100

    tried to add ssl-endpoint-ignoring to get through evil proxies

--- src/org/loklak/harvester/TwitterScraper.java
+++ src/org/loklak/harvester/TwitterScraper.java
@@ -49,25 +49,6 @@ import org.loklak.data.MessageEntry;
 import org.loklak.data.UserEntry;
 import org.loklak.http.ClientConnection;
 
-/**
- *  TwitterScraper
- *  Copyright 08.03.2015 by Michael Peter Christen, @0rb1t3r
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *  
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program in the file lgpl21.txt
- *  If not, see <http://www.gnu.org/licenses/>.
- */
-
 public class TwitterScraper {
 
     public final static ExecutorService executor = Executors.newFixedThreadPool(40);
--- src/org/loklak/http/ClientConnection.java
+++ src/org/loklak/http/ClientConnection.java
@@ -27,6 +27,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -42,18 +48,30 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * Helper class to provide BufferedReader Objects for get and post connections
  */
 public class ClientConnection {
-
+    
     public static String USER_AGENT = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2";
     
     public  static final String CHARSET = "UTF-8";
@@ -68,6 +86,9 @@ public class ClientConnection {
             .setConnectionRequestTimeout(60000)
             .setContentCompressionEnabled(true)
             .build();
+    private static SSLConnectionSocketFactory trustSelfSignedSocketFactory;
+    private static HostnameVerifier trustAllHostsVerifier;
+    private static SSLContext trustAllContext;
     
     private int status;
     public BufferedInputStream inputStream;
@@ -82,6 +103,31 @@ public class ClientConnection {
         cm.setDefaultMaxPerRoute(20);
         HttpHost twitter = new HttpHost("twitter.com", 443);
         cm.setMaxPerRoute(new HttpRoute(twitter), 50);
+        // patch the trust manager to accept all ssl certificates. This will enable us
+    	// to tunnel through http proxies with ssl endpoints (often seen inside company
+    	// intranets and evil environments where someone sniffs on your ssl connecetions).
+    	// With this patch we get out of that no-ssl cage.
+    	// In other environments: don't use that code.
+        try {
+	        trustSelfSignedSocketFactory = new SSLConnectionSocketFactory(
+	        		new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build(),
+	                new String[] {"TLSv1"},
+	                null,
+	                SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+	        trustAllHostsVerifier = new AllowAllHostnameVerifier();
+	        trustAllContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+                public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException { return true;}
+            }).build();
+	        // Install the all-trusting trust manager
+	        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager(){
+	            public X509Certificate[] getAcceptedIssuers(){return null;}
+	            public void checkClientTrusted(X509Certificate[] certs, String authType){}
+	            public void checkServerTrusted(X509Certificate[] certs, String authType){}
+	        }};
+	        SSLContext sc = SSLContext.getInstance("TLS");
+	        sc.init(null, trustAllCerts, new SecureRandom());
+	        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {}
     }
     
     /**
@@ -90,7 +136,13 @@ public class ClientConnection {
      * @throws IOException
      */
     public ClientConnection(String urlstring) throws IOException {
-        this.httpClient = HttpClients.custom().setConnectionManager(cm).setDefaultRequestConfig(defaultRequestConfig).build();
+        this.httpClient = HttpClients.custom()
+	        	.setConnectionManager(cm)
+	        	.setDefaultRequestConfig(defaultRequestConfig)
+        		.setSSLSocketFactory(trustSelfSignedSocketFactory)
+        		.setSSLHostnameVerifier(trustAllHostsVerifier)
+        		.setSSLContext(trustAllContext)
+	        	.build();
         this.request = new HttpGet(urlstring);
         this.request.setHeader("User-Agent", USER_AGENT);
         this.init();
@@ -104,7 +156,13 @@ public class ClientConnection {
      * @throws IOException
      */
     public ClientConnection(String urlstring, Map<String, byte[]> map) throws ClientProtocolException, IOException {
-        this.httpClient = HttpClients.custom().setConnectionManager(cm).setDefaultRequestConfig(defaultRequestConfig).build();
+        this.httpClient = HttpClients.custom()
+        		.setConnectionManager(cm)
+        		.setDefaultRequestConfig(defaultRequestConfig)
+        		.setSSLSocketFactory(trustSelfSignedSocketFactory)
+        		.setSSLHostnameVerifier(trustAllHostsVerifier)
+        		.setSSLContext(trustAllContext)
+        		.build();
         this.request = new HttpPost(urlstring);        
         MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
         entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
@@ -117,6 +175,7 @@ public class ClientConnection {
     }
 
     private void init() throws IOException {
+    	
         this.httpResponse = null;
         try {
             this.httpResponse = httpClient.execute(this.request);
