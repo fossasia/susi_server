9f4c8193cd02c2478a83a288455147649dc91fd5
daminisatya <daminisatya@gmail.com>
2016-04-13 22:57:19 +0530

    Cloud9 setup script

---
+++ cloud9-setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y ant openjdk-7-jdk; git clone https://github.com/loklak/loklak_server.git loklak_server
+cd loklak_server;
+sed -i.bak 's/^\(port.http=\).*/\180/'                conf/config.properties
+sed -i.bak 's/^\(port.https=\).*/\1443/'              conf/config.properties
+sed -i.bak 's/^\(upgradeInterval=\).*/\186400000000/' conf/config.properties
+ant
+bin/start.sh
