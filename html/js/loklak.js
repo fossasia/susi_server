232cd61ec137e31c5588f627e48210c983b9ee5e
Michael Peter Christen <mc@yacy.net>
2016-04-20 10:50:35 +0200

    fix for angular code for statistics from status.json servlet

--- html/js/loklak.js
+++ html/js/loklak.js
@@ -2,7 +2,7 @@ var app = angular.module('loklak', []);
 app.controller("status", function($scope, $http) {
   $http.get('api/status.json').
     success(function(data, status, headers, config) {
-      $scope.index_sizes = data.index_sizes;
+      $scope.index = data.index;
     });
 });
 
