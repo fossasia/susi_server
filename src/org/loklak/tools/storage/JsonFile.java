0a9920259e49f8654d4b7457c243775c7685c0db
treba <treba@localhost.localdomain>
2016-05-23 16:07:50 +0200

    Add gpl license

--- src/org/loklak/tools/storage/JsonFile.java
+++ src/org/loklak/tools/storage/JsonFile.java
@@ -1,3 +1,22 @@
+/**
+ *  JsonFile
+ *  Copyright 22.02.2015 by Robert Mader, @treba123
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.loklak.tools.storage;
 
 import java.io.File;
