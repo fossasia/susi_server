5a651aac44e130a56b176dd59eb1522ce402c051
Leon Mak <leonmak@users.noreply.github.com>
2016-05-27 22:33:24 +0800

    Edited layout to select charts using tabs, and count (#453)

--- html/apps/barchart/app.js
+++ html/apps/barchart/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ngStorage', 'ngMaterial', 'ngMessages' ,'ngSanitize', 'angular-momentjs', 'loklak'])
+angular.module('myApp', ['ngStorage', 'ngMaterial', 'ngMessages' ,'ngSanitize', 'angular-momentjs', 'ui.router', 'ngAnimate','loklak'])
 
 // dataService.polltweets(@querytype, @queryterm) -> polls & adds to $localStorage
 // $localStorage.tweets: stores tweet objs in array
@@ -8,25 +8,27 @@ angular.module('myApp', ['ngStorage', 'ngMaterial', 'ngMessages' ,'ngSanitize', 
 // queryterm: eg. "from:myhandle" (refer to http://loklak.org/api.html for more)
 
 
-.controller('MainCtrl', ['$scope', '$interval', '$localStorage', '$q','$sce', 'queryService',
-    function($scope, $interval, $localStorage, $q, $sce, queryService) {
+.controller('MainCtrl', ['$scope', '$interval', '$localStorage', '$q','$sce', 'queryService', '$location', 
+    function($scope, $interval, $localStorage, $q, $sce, queryService, $location) {
         
         var vm = this;
         // store promise so can cancel on next query
         var promise;
         
+        // settings for form
         $scope.apis = ('search suggest').split(' ').map(function(apitype) {
             return {type: apitype}
-        })
-        
+        })        
         $scope.maxDate = new Date();
         $scope.query = {
             type: 'search',
             term: 'loklak',
             interval: 1,
-            maxHashtags: 3
+            maxHashtags: 3,
+            count: 100
         }
         
+        // storage for processed tweets for charts
         $scope.$storage = $localStorage.$default({
           tweets: [],
           wordFreq: {},
@@ -34,7 +36,20 @@ angular.module('myApp', ['ngStorage', 'ngMaterial', 'ngMessages' ,'ngSanitize', 
           hashtagDateFreq: [],
         })
         // $scope.$storage.tweets=[];
-        window.$scope = $scope;
+        // window.$scope = $scope;
+        
+        // Scope for selected chart
+        $scope.selectedIndex = 0;
+        $scope.$watch('selectedIndex', function(current, old) {
+            switch (current) {
+                case 0:
+                $location.url("/bars");
+                break;
+                case 1:
+                $location.url("/bubbles");
+                break;
+            }
+        });
         
         $scope.handlify = function(handle){
             return $sce.trustAsHtml("<a href=\"https://twitter.com/" + handle + "\">@" + handle + "</a>");
@@ -42,7 +57,7 @@ angular.module('myApp', ['ngStorage', 'ngMaterial', 'ngMessages' ,'ngSanitize', 
         $scope.rmvSpan = function(username) {
             return username.replace(/<img.*?false" alt="(.).*?>/g,"$1").replace(/&nbsp;/g," ");
         };
-        $scope.poll = function(type, term, interval, dateFrom, dateTo, maxHashtags){
+        $scope.poll = function(type, term, interval, dateFrom, dateTo, maxHashtags, count){
             
             // clear previous storage & display & interval
             $scope.$storage.tweets = [];
@@ -58,11 +73,11 @@ angular.module('myApp', ['ngStorage', 'ngMaterial', 'ngMessages' ,'ngSanitize', 
             // $scope.query={};
             
             // first call
-            queryService.queryTweets(type, term, dateFrom, dateTo, maxHashtags);
+            queryService.queryTweets(type, term, dateFrom, dateTo, maxHashtags, count);
 
             // poll every few minutes
             promise = $interval(function(){
-                queryService.queryTweets(type, term, dateFrom, dateTo, maxHashtags);
+                queryService.queryTweets(type, term, dateFrom, dateTo, maxHashtags, count);
             } , interval*60000);
         }
         
@@ -100,4 +115,19 @@ angular.module('myApp', ['ngStorage', 'ngMaterial', 'ngMessages' ,'ngSanitize', 
   $mdThemingProvider.theme('docs-dark', 'default')
     .primaryPalette('yellow')
     .dark();
-});
+})
+
+.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/bars');
+    $stateProvider
+        .state('barChart',{
+            name: 'barChart',
+            url: '/bars',
+            templateUrl: 'charts/barChart.html'
+        })
+        .state('bubbleCloud',{
+            name: 'bubbleCloud',
+            url: '/bubbles',
+            templateUrl: 'charts/bubbleCloud.html'
+        })
+})
