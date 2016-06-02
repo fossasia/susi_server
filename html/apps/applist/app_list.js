4058d777a5f6072f3b90886e3a04215ca8c7a101
daminisatya <daminisatya@gmail.com>
2016-06-02 10:00:33 +0530

    Added categories to Apps page

--- html/apps/applist/app_list.js
+++ html/apps/applist/app_list.js
@@ -1,11 +1,30 @@
 var app = angular.module('appListApp', ['loklak']);
 
 app.controller("app_list", function($scope, $http) {
-  $scope.apps = [];
-  $http.jsonp('/api/apps.json?callback=JSON_CALLBACK')
+    $scope.apps = [];
+    $scope.categoryKeys = [];
+    $http.jsonp('/api/apps.json?callback=JSON_CALLBACK')
     .success(function(data, status, headers, config) {
-      for (var i = 0; i < data.apps.length; i++) {
-        $scope.apps.push(data.apps[i]);
-      }
+        $scope.categoryKeys = data.categories;
+        $scope.apps = data.apps;
+        $scope.categoryKeys.unshift('All');
     });
+
+    $scope.categoryFilter = function(event) {
+        item = event.target.id;
+        if (item != 'All') {
+            $('div.span2').hide();
+            qConstruct = 'div.span2#'+item;
+            $(qConstruct).show();
+        }
+        else {
+            $('div.span2').show();
+        }
+    }
+});
+
+app.filter('nospace', function () {
+    return function (value) {
+        return (!value) ? '' : value.replace(/ /g, '');
+    };
 });
