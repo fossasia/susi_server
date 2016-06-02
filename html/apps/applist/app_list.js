751826b3496bbf1a0d7df723e7bf0ecf3c089fe4
daminisatya <daminisatya@gmail.com>
2016-06-02 21:41:48 +0530

    Added a Dynamic app's name display on apps page

--- html/apps/applist/app_list.js
+++ html/apps/applist/app_list.js
@@ -13,11 +13,14 @@ app.controller("app_list", function($scope, $http) {
     $scope.categoryFilter = function(event) {
         item = event.target.id;
         if (item != 'All') {
+            itemName = item.match(/[A-Z][a-z]+/g);
+            $('#categoryName')[0].innerHTML = itemName.join(" ");
             $('div.span2').hide();
             qConstruct = 'div.span2#'+item;
             $(qConstruct).show();
         }
         else {
+            $('#categoryName')[0].innerHTML = 'Loklak Apps';
             $('div.span2').show();
         }
     }
