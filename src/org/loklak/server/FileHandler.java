817fef3165ed6357dc471226c27b8103f6413a62
Michael Peter Christen <mc@yacy.net>
2016-06-10 07:12:58 +0200

    introduced a RAM cache for small files and html files to further prevent IO. That cache is supposed to be used to compute SSIs as well.

--- src/org/loklak/server/FileHandler.java
+++ src/org/loklak/server/FileHandler.java
@@ -19,7 +19,17 @@
 
 package org.loklak.server;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -30,10 +40,12 @@ import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.PathResource;
 import org.eclipse.jetty.util.resource.Resource;
 
 public class FileHandler extends ResourceHandler implements Handler {
     
+    private final long CACHE_LIMIT = 1024L * 1024L;
     private int expiresSeconds = 0;
     
     /**
@@ -42,6 +54,7 @@ public class FileHandler extends ResourceHandler implements Handler {
      */
     public FileHandler(int expiresSeconds) {
         this.expiresSeconds = expiresSeconds;
+        //this.setMinMemoryMappedContentLength((int) CACHE_LIMIT);
     }
     
     @Override
@@ -81,4 +94,93 @@ public class FileHandler extends ResourceHandler implements Handler {
         // expires: define how long the file shall stay in a cache if cache-control is not used for this information
         response.setDateHeader(HttpHeader.EXPIRES.asString(), System.currentTimeMillis() + expiresSeconds * 1000);
     }
+
+    @Override
+    public Resource getResource(String path) {
+        Resource resource = super.getResource(path);
+        if (resource == null) return null;
+        if (!(resource instanceof PathResource) || !resource.exists()) return resource;
+        try {
+            File f = resource.getFile();
+            if (f.isDirectory()) return resource;
+            CacheResource cache = resourceCache.get(f);
+            if (cache != null) return cache;
+            if (f.length() < CACHE_LIMIT || f.getName().endsWith(".html")) {
+                cache = new CacheResource((PathResource) resource);
+                resourceCache.put(f, cache);
+                return cache;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resource;
+    }
+
+    private final Map<File, CacheResource> resourceCache = new ConcurrentHashMap<>();
+
+    private static class CacheResource extends Resource {
+
+        private byte[] buffer;
+        private long lastModified;
+        private File file;
+        
+        public CacheResource(PathResource pathResource) throws IOException {
+            this.file = pathResource.getFile();
+            initCache(false);
+            pathResource.close();
+        }
+        
+        private void initCache(boolean trueLastModified) throws IOException {
+            this.buffer = Files.readAllBytes(this.file.toPath());
+            this.lastModified = trueLastModified ? this.file.lastModified() : System.currentTimeMillis();
+        }
+
+        @Override
+        public boolean exists() {
+            return true;
+        }
+
+        @Override
+        public boolean isDirectory() {
+            return false;
+        }
+
+        @Override
+        public long lastModified() {
+            if (this.file.lastModified() > this.lastModified) try {initCache(true);} catch (IOException e) {}
+            return this.lastModified;
+        }
+
+        @Override
+        public long length() {
+            return this.buffer.length;
+        }
+
+        @Override
+        public InputStream getInputStream() throws IOException {
+            if (this.file.lastModified() > this.lastModified) initCache(true);
+            return new ByteArrayInputStream(this.buffer);
+        }
+
+        @Override
+        public ReadableByteChannel getReadableByteChannel() throws IOException {
+            return Channels.newChannel(this.getInputStream());
+        }
+
+        @Override
+        public String getName() {
+            return this.file.getName();
+        }
+
+        @Override public void close() {}
+        @Override public String[] list() {throw new UnsupportedOperationException();}
+        @Override public boolean delete() throws SecurityException {throw new UnsupportedOperationException();}
+        @Override public Resource addPath(String arg0) throws IOException, MalformedURLException {throw new UnsupportedOperationException();}
+        @Override public File getFile() throws IOException {throw new UnsupportedOperationException();}
+        @Override public URL getURL() {throw new UnsupportedOperationException();}
+        @Override public boolean isContainedIn(Resource arg0) throws MalformedURLException {throw new UnsupportedOperationException();}
+        @Override public boolean renameTo(Resource arg0) throws SecurityException {throw new UnsupportedOperationException();}
+    }
+    
+    
 }
