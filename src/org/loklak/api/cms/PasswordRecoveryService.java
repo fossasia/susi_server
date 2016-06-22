7623f52d0724c68c3206a5f36727485280c95b51
treba123 <treba@silentundo.org>
2016-06-22 14:11:54 +0200

    permission templates, enheritance of permissions, overrides

--- src/org/loklak/api/cms/PasswordRecoveryService.java
+++ src/org/loklak/api/cms/PasswordRecoveryService.java
@@ -63,6 +63,11 @@ public class PasswordRecoveryService extends AbstractAPIHandler implements APIHa
 	}
 
 	@Override
+	public JSONObject getDefaultPermissions(BaseUserRole baseUserRole) {
+		return null;
+	}
+
+	@Override
 	public JSONObject serviceImpl(Query call, Authorization rights) throws APIException {
 		JSONObject result = new JSONObject();
 
