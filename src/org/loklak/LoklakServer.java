f995059dd8d5decad0f27dd8672c7a8d65be5f09
treba123 <treba@silentundo.org>
2016-07-17 16:21:14 +0200

    Some fixes to make the merge complete Fix for logging to be able to log during shutdown

--- src/org/loklak/LoklakServer.java
+++ src/org/loklak/LoklakServer.java
@@ -36,6 +36,9 @@ import java.util.Set;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.Servlet;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.eclipse.jetty.rewrite.handler.RewriteHandler;
 import org.eclipse.jetty.rewrite.handler.RewriteRegexRule;
 import org.eclipse.jetty.server.Handler;
@@ -269,6 +272,12 @@ public class LoklakServer {
                     TwitterScraper.executor.shutdown();
                     Harvester.executor.shutdown();
                     Log.getLog().info("main terminated, goodby.");
+
+                    if( LogManager.getContext() instanceof LoggerContext) {
+                        Log.getLog().info("Shutting down log4j2");
+                        Configurator.shutdown((LoggerContext)LogManager.getContext());
+                    } else
+                        Log.getLog().warn("Unable to shutdown log4j2");
                 } catch (Exception e) {
                 }
             }
