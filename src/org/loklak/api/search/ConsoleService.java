e6e4ff72a8abc292c18b03b4a96eb54f75ac5a57
jig08 <grover.jigyasa1@gmail.com>
2016-07-21 04:32:34 +0530

    Adding date-time console service..

--- src/org/loklak/api/search/ConsoleService.java
+++ src/org/loklak/api/search/ConsoleService.java
@@ -66,7 +66,8 @@ import org.loklak.tools.storage.JSONObjectWithDefault;
  * http://localhost:9000/api/console.json?q=SELECT%20*%20FROM%20eventbrite%20WHERE%20url=%27https://www.eventbrite.fr/e/billets-europeade-2016-concert-de-musique-vocale-25592599153?aff=es2%27;
  * http://localhost:9000/api/console.json?q=SELECT%20definition,example%20FROM%20urbandictionary%20WHERE%20query=%27football%27;
  * http://localhost:9000/api/console.json?q=SELECT%20*%20FROM%20wordpress%20WHERE%20url=%27https://jigyasagrover.wordpress.com/%27;
- * */
+ * http://localhost:9000/api/console.json?q=SELECT%20*%20FROM%20timeanddate;
+* */
 
 public class ConsoleService extends AbstractAPIHandler implements APIHandler {
    
@@ -223,6 +224,12 @@ public class ConsoleService extends AbstractAPIHandler implements APIHandler {
         });
         dbAccess.put(Pattern.compile("SELECT\\h+?(.*?)\\h+?FROM\\h+?wordpress\\h+?WHERE\\h+?url\\h??=\\h??'(.*?)'\\h??;"), (flow, matcher) -> {
             SusiThought json = WordpressCrawlerService.crawlWordpress(matcher.group(2));
+            SusiTransfer transfer = new SusiTransfer(matcher.group(1));
+            json.setData(transfer.conclude(json.getData()));
+            return json;
+        });
+        dbAccess.put(Pattern.compile("SELECT\\h+?(.*?)\\h+?FROM\\h+?timeanddate;"), (flow, matcher) -> {
+            SusiThought json = TimeAndDateService.timeAndDate();
             SusiTransfer transfer = new SusiTransfer(matcher.group(1));
             json.setData(transfer.conclude(json.getData()));
             return json;
