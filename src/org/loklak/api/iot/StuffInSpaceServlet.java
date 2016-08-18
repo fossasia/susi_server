bf151b6136965834756dc8e9271b24b72a6a320c
sudheesh001 <sudheesh1995@outlook.com>
2016-08-18 23:47:02 +0530

    Fixes #345 - Implements IOT data servlet integrations into loklak
    The commit solves the following IOT integrations
    - Integration of the Earthquake data from US GOV.
    - Scraping and Integrating the data from NOAA for live satellite weather alerts and base stations.
    - Integrating the real time data from the YAHI Singapore to monitor Smog.
    - Integrating the real time public router data from Freifunk Routers worldwide.
    - Integrating the real time Things In Space data into loklak servers for out of world objects.
    - Adds the JSONML file to the java org for markup level metrics and XML Validation to JSON Markup Language
    - Earthquake data can be queried by magnitude and duration parameters live at 5 min harvest intervals

---
+++ src/org/loklak/api/iot/StuffInSpaceServlet.java
@@ -0,0 +1,87 @@
+/**
+ *  Stuff In Space Servlet
+ *  Copyright 10.08.2016 by Sudheesh Singanamalla, @sudheesh001
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.loklak.api.iot;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.io.Reader;
+
+import java.net.*;
+import java.io.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.loklak.server.Query;
+import org.loklak.http.RemoteAccess;
+
+public class StuffInSpaceServlet extends HttpServlet {
+
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();
+	}
+
+	public static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONArray json = new JSONArray(jsonText);
+			return json;
+		} finally {
+			is.close();
+		}
+	}
+
+
+	@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	Query post = RemoteAccess.evaluate(request);
+
+    	// manage DoS
+        if (post.isDoS_blackout()) {response.sendError(503, "your request frequency is too high"); return;}
+
+        String stuffInSpaceUrl = "http://stuffin.space/TLE.json";
+        JSONArray json = readJsonFromUrl(stuffInSpaceUrl);
+
+        PrintWriter sos = response.getWriter();
+        sos.print(json.toString(2));
+        sos.println();
+    }
+
+}
