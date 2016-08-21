0ae0c592e2445af2aafe9811bdc6ea307ac6dfd8
treba123 <treba@silentundo.org>
2016-08-21 13:06:09 +0200

    Add signatures to timelines, no verification yet (have to make changes to peer information stored by loklak)

--- src/org/loklak/tools/JsonSignature.java
+++ src/org/loklak/tools/JsonSignature.java
@@ -17,13 +17,15 @@
  *  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.loklak.tools.storage;
+package org.loklak.tools;
 
 import org.json.JSONObject;
 
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class offers functions to add and verify signatures of JSONObjects
@@ -58,12 +60,35 @@ public class JsonSignature {
         obj.put(signatureString, new String(Base64.getEncoder().encode(sigBytes)));
     }
 
+    public static void addSignature(Map<String, byte[]> obj, PrivateKey key) throws InvalidKeyException, SignatureException {
+
+        removeSignature(obj);
+
+        Signature signature;
+        try {
+            signature = Signature.getInstance("SHA256withRSA");
+        } catch (NoSuchAlgorithmException e) {
+            return; //does not happen
+        }
+
+        signature.initSign(key);
+        signature.update(obj.toString().getBytes(StandardCharsets.UTF_8));
+
+        byte[] sigBytes = signature.sign();
+
+        obj.put(signatureString, Base64.getEncoder().encode(sigBytes));
+    }
+
     /**
      * Remove the signature
      * @param obj the JSONObject
      */
     public static void removeSignature(JSONObject obj){
         if(obj.has(signatureString)) obj.remove(signatureString);
+    }
+
+    public static void removeSignature(Map<String, byte[]> obj){
+        if(obj.containsKey(signatureString)) obj.remove(signatureString);
     }
 
     /**
@@ -98,8 +123,36 @@ public class JsonSignature {
         return res;
     }
 
+    public static boolean verify(Map<String, byte[]> obj, PublicKey key) throws SignatureException, InvalidKeyException {
+
+        if(!obj.containsKey(signatureString)) throw new SignatureException("No signature supplied");
+
+        Signature signature;
+        try {
+            signature = Signature.getInstance("SHA256withRSA");
+        } catch (NoSuchAlgorithmException e) {
+            return false; //does not happen
+        }
+
+        byte[] sigString = obj.get(signatureString);
+        byte[] sig = Base64.getDecoder().decode(sigString);
+        obj.remove(signatureString);
+
+        signature.initVerify(key);
+        signature.update(obj.toString().getBytes(StandardCharsets.UTF_8));
+        boolean res = signature.verify(sig);
+
+        obj.put(signatureString, sigString);
+
+        return res;
+    }
+
     public static boolean hasSignature(JSONObject obj){
         return obj.has(signatureString);
+    }
+
+    public static boolean hasSignature(Map<String, byte[]> obj){
+        return obj.containsKey(signatureString);
     }
 
     public static void main(String[] args) throws Exception {
@@ -107,7 +160,7 @@ public class JsonSignature {
         keyGen.initialize(2048);
         KeyPair keyPair = keyGen.genKeyPair();
 
-        JSONObject randomObj = new JSONObject("{\n" +
+        String jsonString = "{\n" +
                 "        \"_id\": \"57b44e738d9af9fa2df13b27\",\n" +
                 "        \"index\": 0,\n" +
                 "        \"guid\": \"13af6838-08c8-4709-8dff-5ecb20bbaaa7\",\n" +
@@ -151,10 +204,42 @@ public class JsonSignature {
                 "        ],\n" +
                 "        \"greeting\": \"Hello, Wyatt Jefferson! You have 1 unread messages.\",\n" +
                 "        \"favoriteFruit\": \"strawberry\"\n" +
-                "    }");
+                "    }";
+
+        String jsonStringSimple = "{\n" +
+                "        \"_id\": \"57b44e738d9af9fa2df13b27\",\n" +
+                "        \"index\": 0,\n" +
+                "        \"guid\": \"13af6838-08c8-4709-8dff-5ecb20bbaaa7\",\n" +
+                "        \"isActive\": false,\n" +
+                "        \"balance\": \"$2,092.08\",\n" +
+                "        \"picture\": \"http://placehold.it/32x32\",\n" +
+                "        \"age\": 22,\n" +
+                "        \"eyeColor\": \"blue\",\n" +
+                "        \"name\": \"Wyatt Jefferson\",\n" +
+                "        \"gender\": \"male\",\n" +
+                "        \"company\": \"GEEKFARM\",\n" +
+                "        \"email\": \"wyattjefferson@geekfarm.com\",\n" +
+                "        \"phone\": \"+1 (855) 405-2375\",\n" +
+                "        \"address\": \"506 Court Street, Gambrills, Minnesota, 8953\",\n" +
+                "        \"about\": \"Ea sunt quis non occaecat aliquip sint eiusmod. Aliquip id non ut sunt est laboris proident reprehenderit incididunt velit. Quis deserunt dolore aliqua voluptate magna laborum minim. Pariatur voluptate ad consequat culpa sit veniam eiusmod et ex ipsum.\\r\\n\",\n" +
+                "        \"registered\": \"2015-08-08T03:21:53 -02:00\",\n" +
+                "        \"latitude\": -39.880621,\n" +
+                "        \"longitude\": 44.053688,\n" +
+                "    }";
+
+        JSONObject randomObj = new JSONObject(jsonString);
+        JSONObject tmp = new JSONObject(jsonStringSimple);
+        Map<String, byte[]> randomObj2 = new HashMap<String, byte[]>();
+        for(String key : tmp.keySet()){
+            Object value = tmp.get(key);
+            randomObj2.put(key, value.toString().getBytes());
+        }
 
         addSignature(randomObj,keyPair.getPrivate());
-        if(hasSignature(randomObj)) System.out.println("Verify: " + verify(randomObj,keyPair.getPublic()));
+        addSignature(randomObj2,keyPair.getPrivate());
+        if(hasSignature(randomObj)) System.out.println("Verify 1: " + verify(randomObj,keyPair.getPublic()));
+        if(hasSignature(randomObj2)) System.out.println("Verify 2: " + verify(randomObj,keyPair.getPublic()));
         removeSignature(randomObj);
+        removeSignature(randomObj2);
     }
 }
