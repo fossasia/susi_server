fff32a1958e186c1a999d7ab53a4230c15c4695d
Michael Peter Christen <mc@yacy.net>
2016-10-17 11:11:56 +0200

    added test cases for susi

---
+++ test/org/loklak/susi/SusiArgumentTest.java
@@ -0,0 +1,43 @@
+package org.loklak.susi;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SusiArgumentTest {
+
+    public static SusiArgument generateTestArgument(String key, String value) {
+        return new SusiArgument().think(SusiThoughtTest.generateTestThought(key, value));
+    }
+    
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        SusiArgument a = generateTestArgument("a", "22");
+        a.think(SusiThoughtTest.generateTestThought("b", "33"));
+        SusiThought t = a.mindmeld(true);
+        assertTrue(t.getCount() == 1);
+        a.think(SusiThoughtTest.generateTestThought("b", "44"));
+        assertTrue(a.mindmeld(true).getObservations("b").get(0).equals("44"));
+    }
+
+}
