93f9016b02bea0cde3844d0d417b4b0e3f29c17e
Fabian Lüpke <fabian.luepke@gmail.com>
2017-05-21 17:23:51 +0200

    Add deliveroo API as an external API

--- test/ai/susi/geo/GeocodingTest.java
+++ test/ai/susi/geo/GeocodingTest.java
@@ -30,11 +30,39 @@ public class GeocodingTest {
 	}
 	
 	@Test
-	public void testDistance() {
+	public void testDistanceDouble() {
 		IntegerGeoPoint pointA = new IntegerGeoPoint(52.0,13.0);
 		IntegerGeoPoint pointB = new IntegerGeoPoint(53.0,14.0);
 		double distanceKM = pointA.distance(pointB) / 1000;
 		assertTrue(distanceKM > 130);
 		assertTrue(distanceKM < 131);
+	}
+	
+	@Test
+	public void testAccuracy() {
+		IntegerGeoPoint point = new IntegerGeoPoint(52.516389, 13.000009);
+		System.out.println(point.lat());
+		assertTrue(point.lat() > 52.516389 - point.accuracyLat());
+		assertTrue(point.lat() < 52.516389 + point.accuracyLat());
+		assertTrue(point.lon() > 13.000009 - point.accuracyLon());
+		assertTrue(point.lon() < 13.000009 + point.accuracyLon());
+	}
+	
+	@Test
+	public void testDistanceInteger() {
+		IntegerGeoPoint pointA = new IntegerGeoPoint(52516300, 13377700);
+		IntegerGeoPoint pointB = new IntegerGeoPoint(54000000, 14000000);
+		double distanceKM = pointA.distance(pointB) / 1000;
+		System.out.println(pointA);
+		assertTrue(distanceKM > 170);
+		assertTrue(distanceKM < 171);
+	}
+	
+	@Test
+	public void testEquality() {
+		IntegerGeoPoint pointA = new IntegerGeoPoint(52.5162746, 13.377704);
+		IntegerGeoPoint pointB = new IntegerGeoPoint(52.5162746, 13.377704);
+		assertTrue(pointA.equals(pointB));
+		assertTrue(pointB.hashCode() == pointB.hashCode());
 	}
 }
