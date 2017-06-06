17fec10856c0635232c5d977e027e688b3b6f53d
DravitLochan <dlochangupta@gmail.com>
2017-06-06 19:10:33 +0530

    encoding and group array generated

--- src/ai/susi/server/api/cms/GroupListService.java
+++ src/ai/susi/server/api/cms/GroupListService.java
@@ -1,10 +1,13 @@
 package ai.susi.server.api.cms;
 
+import ai.susi.DAO;
 import ai.susi.json.JsonObjectWithDefault;
 import ai.susi.server.*;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 
 /**
  * Created by DravitLochan on 6/6/17.
@@ -26,7 +29,13 @@ public class GroupListService extends AbstractAPIHandler implements APIHandler{
     }
 
     @Override
-    public ServiceResponse serviceImpl(Query post, HttpServletResponse response, Authorization rights, JsonObjectWithDefault permissions) throws APIException {
-        return null;
+    public ServiceResponse serviceImpl(Query call, HttpServletResponse response, Authorization rights, JsonObjectWithDefault permissions) throws APIException {
+
+        String model_name = call.get("model", "general");
+        File model = new File(DAO.model_watch_dir, model_name);
+
+        String[] groups = model.list();
+        JSONArray groupsArray = new JSONArray(groups);
+        return new ServiceResponse(groupsArray);
     }
 }
