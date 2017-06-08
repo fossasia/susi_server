954661d8c0a5c932d3712145bef70fe947d653bb
Chetan Kaushik <dynamitechetan@gmail.com>
2017-06-08 16:23:27 +0530

    Add Hisorization | Add gradle dependency for JGit

--- src/ai/susi/server/api/cms/DeleteExpertService.java
+++ src/ai/susi/server/api/cms/DeleteExpertService.java
@@ -3,10 +3,15 @@ package ai.susi.server.api.cms;
 import ai.susi.DAO;
 import ai.susi.json.JsonObjectWithDefault;
 import ai.susi.server.*;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by chetankaushik on 06/06/17.
@@ -47,10 +52,38 @@ public class DeleteExpertService  extends AbstractAPIHandler implements APIHandl
         if (expert.exists()) {
             expert.delete();
             json.put("deleted_file",ExpertName);
+
+            //Add to git
+            FileRepositoryBuilder builder = new FileRepositoryBuilder();
+            Repository repository = null;
+            try {
+                repository = builder.setGitDir((DAO.susi_skill_repo))
+                        .readEnvironment() // scan environment GIT_* variables
+                        .findGitDir() // scan up the file system tree
+                        .build();
+
+            try (Git git = new Git(repository)) {
+                git.add()
+                        .addFilepattern(expert_name)
+                        .call();
+                // and then commit the changes
+                git.commit()
+                        .setMessage("Deleted "+ expert_name)
+                        .call();
+
+            } catch (GitAPIException e) {
+                e.printStackTrace();
+            }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         } else {
             json.put("Error","Cannot find '" + expert + "' ('" + expert.getAbsolutePath() + "')");
         }
+        
         return new ServiceResponse(json);
 
     }
+
 }
