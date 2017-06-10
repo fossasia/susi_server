8ca51f9bd1ce2a25008ef875104f396431065021
DravitLochan <dlochangupta@gmail.com>
2017-06-10 23:31:58 +0530

    required servlet methods

--- src/ai/susi/server/api/aaa/DownloadDataSettings.java
+++ src/ai/susi/server/api/aaa/DownloadDataSettings.java
@@ -1,7 +1,21 @@
 package ai.susi.server.api.aaa;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by dravit on 10/6/17.
  */
-public class DownloadDataSettings {
+public class DownloadDataSettings extends HttpServlet{
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        
+    }
 }
