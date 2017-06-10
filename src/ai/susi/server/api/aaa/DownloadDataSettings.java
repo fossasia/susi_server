499f4eeca9a790d8f5250875973de1736df7bdf3
DravitLochan <dlochangupta@gmail.com>
2017-06-11 00:30:31 +0530

    traverse to settings directory

--- src/ai/susi/server/api/aaa/DownloadDataSettings.java
+++ src/ai/susi/server/api/aaa/DownloadDataSettings.java
@@ -1,9 +1,12 @@
 package ai.susi.server.api.aaa;
 
+import ai.susi.DAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -16,6 +19,18 @@ public class DownloadDataSettings extends HttpServlet{
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        
+
+        try{
+            File settings = new File(DAO.data_dir.getPath()+"/settings");
+            String path = settings.getPath();
+            System.out.println(path);
+            String[] files = settings.list();
+            System.out.println(files.length);
+            
+        }
+        catch (Exception e){
+
+        }
+
     }
 }
