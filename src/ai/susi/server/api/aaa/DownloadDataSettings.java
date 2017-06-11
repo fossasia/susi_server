37bab63428dd99c1a9d6b734977d98737a5d5d05
DravitLochan <dlochangupta@gmail.com>
2017-06-12 01:06:10 +0530

    file sent from server to client making the request

--- src/ai/susi/server/api/aaa/DownloadDataSettings.java
+++ src/ai/susi/server/api/aaa/DownloadDataSettings.java
@@ -1,15 +1,22 @@
 package ai.susi.server.api.aaa;
 
+import ai.susi.DAO;
 import ai.susi.json.JsonObjectWithDefault;
 import ai.susi.server.*;
 import org.json.JSONObject;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 /**
  * Created by dravit on 10/6/17.
  */
 public class DownloadDataSettings extends AbstractAPIHandler implements APIHandler{
+
+    private static final int BUFSIZE = 4096;
+
     @Override
     public String getAPIPath() {
         return "/data/settings";
@@ -17,7 +24,7 @@ public class DownloadDataSettings extends AbstractAPIHandler implements APIHandl
 
     @Override
     public BaseUserRole getMinimalBaseUserRole() {
-        return null;
+        return BaseUserRole.ADMIN;
     }
 
     @Override
@@ -27,6 +34,38 @@ public class DownloadDataSettings extends AbstractAPIHandler implements APIHandl
 
     @Override
     public ServiceResponse serviceImpl(Query post, HttpServletResponse response, Authorization rights, JsonObjectWithDefault permissions) throws APIException {
+
+        File settings = new File(DAO.data_dir.getPath()+"/settings");
+        String filePath = settings.getPath();
+        String fileName = post.get("file","accounting");
+        filePath += "/"+fileName+".json";
+        System.out.println(filePath);
+        int length   = 0;
+        File file = new File(filePath);
+        try{
+            ServletOutputStream outStream = response.getOutputStream();
+            ServletContext context  = getServletConfig().getServletContext();
+            String mimetype = context.getMimeType(filePath);
+
+            if (mimetype == null) {
+                mimetype = "application/octet-stream";
+            }
+            response.setContentType(mimetype);
+            response.setContentLength((int)file.length());
+            response.setHeader("Content-Disposition", "attachment; filename=" + fileName +".json");
+            byte[] byteBuffer = new byte[BUFSIZE];
+            DataInputStream in = new DataInputStream(new FileInputStream(file));
+            while ((in != null) && ((length = in.read(byteBuffer)) != -1))
+            {
+                outStream.write(byteBuffer,0,length);
+            }
+
+            in.close();
+            outStream.close();
+        }
+        catch (Exception e){
+            System.out.println(e.getCause()+"\n"+e.getMessage());
+        }
         return null;
     }
 }
