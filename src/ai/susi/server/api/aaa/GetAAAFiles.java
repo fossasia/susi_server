0271a6987fbd40bd25f1cfb4d4c5eb0e7e0ec078
DravitLochan <dlochangupta@gmail.com>
2017-06-14 00:14:35 +0530

    defined end point and documented

--- src/ai/susi/server/api/aaa/GetAAAFiles.java
+++ src/ai/susi/server/api/aaa/GetAAAFiles.java
@@ -8,11 +8,12 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by dravit on 14/6/17.
+ * Servlet to get list of all the files present in data/settings directory
  */
 public class GetAAAFiles extends AbstractAPIHandler implements APIHandler{
     @Override
     public String getAPIPath() {
-        return null;
+        return "/aaa/getAllFiles";
     }
 
     @Override
