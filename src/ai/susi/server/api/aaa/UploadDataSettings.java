5041a2d311af49fefb6d94e0dd9306986d5f03c9
DravitLochan <dlochangupta@gmail.com>
2017-06-13 09:02:07 +0530

    json file uploads to server and saved in data/setings directory

--- src/ai/susi/server/api/aaa/UploadDataSettings.java
+++ src/ai/susi/server/api/aaa/UploadDataSettings.java
@@ -1,11 +1,16 @@
 package ai.susi.server.api.aaa;
 
+import ai.susi.DAO;
 import ai.susi.json.JsonObjectWithDefault;
 import ai.susi.server.*;
 import org.json.JSONObject;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -20,7 +25,7 @@ public class UploadDataSettings extends AbstractAPIHandler implements APIHandler
 
     @Override
     public BaseUserRole getMinimalBaseUserRole() {
-        return null;
+        return BaseUserRole.ADMIN;
     }
 
     @Override
@@ -30,12 +35,60 @@ public class UploadDataSettings extends AbstractAPIHandler implements APIHandler
 
     @Override
     public ServiceResponse serviceImpl(Query post, HttpServletResponse response, Authorization rights, JsonObjectWithDefault permissions) throws APIException {
+
+        File data = DAO.data_dir;
+        String filePath = data.getPath() + "/settings/";
+        String fileName = (String) post.getRequest().getParameter("file");
+        if (fileName == null || fileName.equals(""))
+            try {
+                throw new ServletException("Invalid or non-existent file parameter in SendWord servlet.");
+            } catch (ServletException e) {
+                e.printStackTrace();
+            }
+        // add the .doc suffix if it doesn't already exist
+        if (fileName.indexOf(".json") == -1)
+            fileName = fileName + ".json";
+
+        String wordDir = getServletContext().getInitParameter("word-dir");
+        if (wordDir == null || wordDir.equals(""))
+            try {
+                throw new ServletException("Invalid or non-existent wordDir context-param.");
+            } catch (ServletException e) {
+                e.printStackTrace();
+            }
+        ServletOutputStream stream = null;
+        BufferedInputStream buf = null;
         try {
-            doPost(post.getRequest(), response);
-        } catch (ServletException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            stream = response.getOutputStream();
+            File file = new File(filePath +  fileName);
+            response.setContentType("application/json");
+            response.addHeader("Content-Disposition", "attachment; filename="
+                    + fileName);
+            response.setContentLength((int) file.length());
+            FileInputStream input = new FileInputStream(file);
+            buf = new BufferedInputStream(input);
+            int readBytes = 0;
+            while ((readBytes = buf.read()) != -1)
+                stream.write(readBytes);
+        } catch (IOException ioe) {
+            try {
+                throw new ServletException(ioe.getMessage());
+            } catch (ServletException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            if (stream != null)
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            if (buf != null)
+                try {
+                    buf.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
         }
         return null;
     }
