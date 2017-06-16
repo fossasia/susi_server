6f6e0f55ce620fc6f0add1c8da5c9d8edd3216b0
Chetan Kaushik <dynamitechetan@gmail.com>
2017-06-16 12:39:47 +0530

    Added serialVersionUID in GetGroupMembers.java, Indentation fixes

--- src/ai/susi/server/api/cms/GetGroupMembers.java
+++ src/ai/susi/server/api/cms/GetGroupMembers.java
@@ -19,10 +19,13 @@ import java.util.Set;
  */
 public class GetGroupMembers extends AbstractAPIHandler implements APIHandler {
 
-    private static final long serialVersionUID = -8691003678852307876L;
+
+    private static final long serialVersionUID = 5747506850176916431L;
 
     @Override
-    public BaseUserRole getMinimalBaseUserRole() { return BaseUserRole.ANONYMOUS; }
+    public BaseUserRole getMinimalBaseUserRole() {
+        return BaseUserRole.ANONYMOUS;
+    }
 
     @Override
     public JSONObject getDefaultPermissions(BaseUserRole baseUserRole) {
@@ -38,40 +41,37 @@ public class GetGroupMembers extends AbstractAPIHandler implements APIHandler {
     public ServiceResponse serviceImpl(Query call, HttpServletResponse response, Authorization rights, final JsonObjectWithDefault permissions) {
         Boolean foundUser;
         JSONObject success = new JSONObject();
-        success.put("success",false);
-        JSONObject allUsers ;
+        success.put("success", false);
+        JSONObject allUsers;
         allUsers = DAO.group;
         String model_name = call.get("group", null);
         foundUser = false;
-        if(model_name==null){
+        if (model_name == null) {
             return new ServiceResponse(success);
-        }
-
-        else{
-            Set<?> s =  allUsers.keySet();
+        } else {
+            //Searching for keys in groups.json
+            Set<?> s = allUsers.keySet();
 
             Iterator<?> i = s.iterator();
-            do{
+            do {
                 String k = i.next().toString();
 
-                if(model_name.equals(k)){
-                    foundUser=true;
+                if (model_name.equals(k)) {
+                    foundUser = true;
                     break;
                 }
 
 
-            }while(i.hasNext());
+            } while (i.hasNext());
 
-            if(foundUser) {
+            if (foundUser) {
                 JSONObject details = new JSONObject();
                 details = allUsers.getJSONObject(model_name);
-                details.put("success",true);
+                details.put("success", true);
                 return new ServiceResponse(details);
 
-            }
-            else
+            } else
                 return new ServiceResponse(success);
-
 
 
         }
