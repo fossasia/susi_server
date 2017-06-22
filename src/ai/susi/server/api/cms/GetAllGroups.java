b33d3e6787037c26e357a5f3132d2499e3e1bd87
Chetan Kaushik <dynamitechetan@gmail.com>
2017-06-22 22:22:28 +0530

    Added API Endpoint to get all Groups

---
+++ src/ai/susi/server/api/cms/GetAllGroups.java
@@ -0,0 +1,46 @@
+package ai.susi.server.api.cms;
+
+import ai.susi.DAO;
+import ai.susi.json.JsonObjectWithDefault;
+import ai.susi.server.*;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by chetankaushik on 22/06/17.
+ */
+public class GetAllGroups extends AbstractAPIHandler implements APIHandler {
+
+
+    private static final long serialVersionUID = 5747506850176916431L;
+
+    @Override
+    public BaseUserRole getMinimalBaseUserRole() {
+        return BaseUserRole.ANONYMOUS;
+    }
+
+    @Override
+    public JSONObject getDefaultPermissions(BaseUserRole baseUserRole) {
+        return null;
+    }
+
+    @Override
+    public String getAPIPath() {
+        return "/cms/getGroupDetails.json";
+    }
+
+    @Override
+    public ServiceResponse serviceImpl(Query call, HttpServletResponse response, Authorization rights, final JsonObjectWithDefault permissions) {
+        JSONObject success = new JSONObject();
+        success.put("success", false);
+        JSONObject allUsers;
+        allUsers = DAO.group.toJSON();
+        String model_name = call.get("group", null);
+        allUsers.put("success", true);
+        return new ServiceResponse(allUsers);
+    }
+
+}
