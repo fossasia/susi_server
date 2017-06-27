886f1dfb8018781bf10e08a907adb145dbbe345f
DravitLochan <dlochangupta@gmail.com>
2017-06-27 10:35:34 +0530

    header and servlet documentation

--- src/ai/susi/server/api/aaa/DeleteGroupService.java
+++ src/ai/susi/server/api/aaa/DeleteGroupService.java
@@ -1,3 +1,22 @@
+/**
+ *  DeleteGroupService
+ *  Copyright 27/6/17 by Dravit Lochan, @DravitLochan
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ai.susi.server.api.aaa;
 
 import ai.susi.json.JsonObjectWithDefault;
@@ -8,6 +27,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by dravit on 27/6/17.
+ * http://127.0.0.1:4000/aaa/deleteGroup.json?group=groupName
  */
 public class DeleteGroupService extends AbstractAPIHandler implements APIHandler{
     @Override
