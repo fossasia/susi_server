9c644126e233b1bcc32190ffecd1b1fb1ffc5f3e
DravitLochan <dlochangupta@gmail.com>
2017-06-27 10:22:05 +0530

    basic file structure

---
+++ src/ai/susi/server/api/aaa/DeleteGroupService.java
@@ -0,0 +1,32 @@
+package ai.susi.server.api.aaa;
+
+import ai.susi.json.JsonObjectWithDefault;
+import ai.susi.server.*;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by dravit on 27/6/17.
+ */
+public class DeleteGroupService extends AbstractAPIHandler implements APIHandler{
+    @Override
+    public String getAPIPath() {
+        return null;
+    }
+
+    @Override
+    public BaseUserRole getMinimalBaseUserRole() {
+        return null;
+    }
+
+    @Override
+    public JSONObject getDefaultPermissions(BaseUserRole baseUserRole) {
+        return null;
+    }
+
+    @Override
+    public ServiceResponse serviceImpl(Query post, HttpServletResponse response, Authorization rights, JsonObjectWithDefault permissions) throws APIException {
+        return null;
+    }
+}
