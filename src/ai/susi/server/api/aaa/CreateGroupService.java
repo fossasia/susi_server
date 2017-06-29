a70de3eada4a440d154d9488034d6aea83f16de4
Chetan Kaushik <dynamitechetan@gmail.com>
2017-06-29 17:04:50 +0530

    Changed Typo in URL

--- src/ai/susi/server/api/aaa/CreateGroupService.java
+++ src/ai/susi/server/api/aaa/CreateGroupService.java
@@ -34,7 +34,7 @@ import javax.servlet.http.HttpServletResponse;
  * This Servlets creates a group with default baserole as anonymous.
  * It takes 1 parameter group and other optional parameter role.
  * Can be tested on
- * http://127.0.0.1:4000/aaa/createGroups.json?group=groupName&role=admin
+ * http://127.0.0.1:4000/aaa/createGroup.json?group=groupName&role=admin
  *
  */
 public class CreateGroupService extends AbstractAPIHandler implements APIHandler {
