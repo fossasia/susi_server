85e9d7d2cfcd1af20a3d78f7bbfbe9420a33409e
Chetan Kaushik <dynamitechetan@gmail.com>
2017-07-06 18:17:32 +0530

    Added Endpoint to get Skill Text in JSON Format
    Added Endpoint to get Skill Text in JSON Format.

--- src/ai/susi/server/api/cms/GetSkillJsonService.java
+++ src/ai/susi/server/api/cms/GetSkillJsonService.java
@@ -6,12 +6,12 @@
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- *  
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program in the file lgpl21.txt
  *  If not, see <http://www.gnu.org/licenses/>.
@@ -19,6 +19,7 @@
 
 package ai.susi.server.api.cms;
 
+import ai.susi.DAO;
 import org.json.JSONObject;
 
 import ai.susi.json.JsonObjectWithDefault;
@@ -30,9 +31,12 @@ import ai.susi.server.Query;
 import ai.susi.server.ServiceResponse;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class GetSkillJsonService extends AbstractAPIHandler implements APIHandler {
-    
+
     private static final long serialVersionUID = 18344223L;
 
     @Override
@@ -47,14 +51,31 @@ public class GetSkillJsonService extends AbstractAPIHandler implements APIHandle
     public String getAPIPath() {
         return "/cms/getSkill.json";
     }
-    
+
     @Override
     public ServiceResponse serviceImpl(Query call, HttpServletResponse response, Authorization rights, final JsonObjectWithDefault permissions) {
-        
+
         JSONObject json = new JSONObject(true);
-        
+
         // modify caching
         json.put("$EXPIRES", 0);
-        return new ServiceResponse(json);
+
+        String model_name = call.get("model", "general");
+        File model = new File(DAO.model_watch_dir, model_name);
+        String group_name = call.get("group", "knowledge");
+        File group = new File(model, group_name);
+        String language_name = call.get("language", "en");
+        File language = new File(group, language_name);
+        String skill_name = call.get("skill", "wikipedia");
+        File skill = new File(language, skill_name + ".txt");
+
+        try {
+            String content = new String(Files.readAllBytes(skill.toPath()));
+            json.put("text",content);
+            return new ServiceResponse(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
