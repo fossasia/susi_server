f97e6dfa9dbbcf6bea3c6b0dc10104ed5f9a0082
DravitLochan <dlochangupta@gmail.com>
2017-07-07 01:24:08 +0530

    hash check. if true, assign new password

--- src/ai/susi/server/api/aaa/PasswordChangeService.java
+++ src/ai/susi/server/api/aaa/PasswordChangeService.java
@@ -18,9 +18,12 @@
  */
 package ai.susi.server.api.aaa;
 
+import ai.susi.DAO;
 import ai.susi.json.JsonObjectWithDefault;
 import ai.susi.server.*;
+import org.eclipse.jetty.util.log.Log;
 import org.json.JSONObject;
+
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -30,7 +33,7 @@ import javax.servlet.http.HttpServletResponse;
 public class PasswordChangeService extends AbstractAPIHandler implements APIHandler{
     @Override
     public String getAPIPath() {
-        return "/aaa/resetpassword.json";
+        return "/aaa/changepassword.json";
     }
 
     @Override
@@ -45,6 +48,43 @@ public class PasswordChangeService extends AbstractAPIHandler implements APIHand
 
     @Override
     public ServiceResponse serviceImpl(Query post, HttpServletResponse response, Authorization rights, JsonObjectWithDefault permissions) throws APIException {
+        JSONObject result = new JSONObject();
+
+        String useremail = post.get("changepassword", null);
+        String password = post.get("password", null);
+        String newpassword = post.get("newpassword",null);
+
+        ClientCredential credential = new ClientCredential(ClientCredential.Type.change_pass, useremail);
+        ClientIdentity identity = new ClientIdentity(ClientIdentity.Type.email, credential.getName());
+        Authentication authentication = DAO.getAuthentication(credential);
+
+        String passwordHash;
+        String salt;
+
+        try {
+            passwordHash = authentication.getString("passwordHash");
+            salt = authentication.getString("salt");
+        } catch (Throwable e) {
+            Log.getLog().info("Invalid password try for user: " + identity.getName() + " from host: " + post.getClientHost() + " : password or salt missing in database");
+            throw new APIException(422, "Invalid credentials");
+        }
+        if (!passwordHash.equals(getHash(password, salt))) {
+
+            // save invalid login in accounting object
+            Accounting accouting = DAO.getAccounting(identity);
+            accouting.getRequests().addRequest(this.getClass().getCanonicalName(), "invalid login");
+
+            Log.getLog().info("Invalid change password try for user: " + identity.getName() + " via passwd from host: " + post.getClientHost());
+            throw new APIException(422, "Invalid credentials");
+        } else {
+            String newSalt = createRandomString(20);
+            authentication.remove("salt");
+            authentication.remove("passwordHash");
+            authentication.put("salt", salt);
+            authentication.put("passwordHash", getHash(newpassword, salt));
+        }
+
         return null;
     }
+
 }
