82279da585fd6db0e79a6087b46d2610509622d2
cc15 <chiragwadhera15@gmail.com>
2017-07-24 11:28:08 +0530

    Removing git history of sub module

--- kubernetes/images/setup.sh
+++ kubernetes/images/setup.sh
@@ -3,12 +3,15 @@
 # clone the github repo
 git clone https://github.com/fossasia/susi_server.git /susi_server
 cd /susi_server
-git submodule update --recursive --remote
-git submodule update --init --recursive
+
 git checkout ${BRANCH}
 
 if [ -v COMMIT_HASH ]; then
     git reset --hard ${COMMIT_HASH}
 fi
 
+git submodule update --recursive --remote
+git submodule update --init --recursive
+
+rm -rf dependencies/public-transport-enabler/.git
 rm -rf .git
