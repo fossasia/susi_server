dc714b4dfb8ed87337495d68c25b5f41ca424948
DravitLochan <dlochangupta@gmail.com>
2017-07-25 10:16:26 +0530

    removed unwanted <p> tag

--- src/ai/susi/server/api/aaa/PasswordChangeService.java
+++ src/ai/susi/server/api/aaa/PasswordChangeService.java
@@ -1,17 +1,14 @@
 /**
  * PasswordResetService
  * Copyright 6/7/17 by Dravit Lochan, @DravitLochan
- * <p>
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * <p>
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program in the file lgpl21.txt
  * If not, see <http://www.gnu.org/licenses/>.
