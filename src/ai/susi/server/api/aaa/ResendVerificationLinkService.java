74c76b1a88caf575622218495b5ece155460c853
DravitLochan <dlochangupta@gmail.com>
2017-07-26 10:43:25 +0530

    defined end point, base user role and header

---
+++ src/ai/susi/server/api/aaa/ResendVerificationLinkService.java
@@ -0,0 +1,52 @@
+/**
+ *  PasswordResetService
+ *  Copyright 25/7/17 by Dravit Lochan, @DravitLochan
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ai.susi.server.api.aaa;
+
+import ai.susi.json.JsonObjectWithDefault;
+import ai.susi.server.*;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by dravit on 25/7/17.
+ */
+public class ResendVerificationLinkService extends AbstractAPIHandler implements APIHandler{
+    @Override
+    public String getAPIPath() {
+        return "/aaa/resendverificatiionlink.json";
+    }
+
+    @Override
+    public BaseUserRole getMinimalBaseUserRole() {
+        return BaseUserRole.ANONYMOUS;
+    }
+
+    @Override
+    public JSONObject getDefaultPermissions(BaseUserRole baseUserRole) {
+        return null;
+    }
+
+    @Override
+    public ServiceResponse serviceImpl(Query post, HttpServletResponse response, Authorization rights, JsonObjectWithDefault permissions) throws APIException {
+
+        JSONObject json = new JSONObject(true);
+        return null;
+    }
+}
