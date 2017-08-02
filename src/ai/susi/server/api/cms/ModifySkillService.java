39c32e7a9cac01018f4974d72e6df0c70d80816b
Chetan Kaushik <dynamitechetan@gmail.com>
2017-08-02 09:47:40 +0530

    Changed base role to admin

--- src/ai/susi/server/api/cms/ModifySkillService.java
+++ src/ai/susi/server/api/cms/ModifySkillService.java
@@ -27,7 +27,7 @@ public class ModifySkillService extends AbstractAPIHandler implements APIHandler
     private static final long serialVersionUID = -1834363513093189312L;
 
     @Override
-    public BaseUserRole getMinimalBaseUserRole() { return BaseUserRole.USER; }
+    public BaseUserRole getMinimalBaseUserRole() { return BaseUserRole.ADMIN; }
 
     @Override
     public JSONObject getDefaultPermissions(BaseUserRole baseUserRole) {
