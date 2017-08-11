fd5c6c9248869f26ddfd423dde4496b05289a38c
dynamitechetan <dynamitechetan@gmail.com>
2017-08-11 14:42:36 +0530

    Showing the file at a commit in JSON response

--- src/ai/susi/server/api/cms/GetFileAtCommitID.java
+++ src/ai/susi/server/api/cms/GetFileAtCommitID.java
@@ -15,15 +15,17 @@ import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.filter.PathFilter;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by chetankaushik on 10/08/17.
+ * Returns the file at a specific commitID. similar to Git Show.
+ * http://127.0.0.1:4000/cms/getFileAtCommitID.json?model=general&group=knowledge&language=en&skill=bitcoin&commitID=214791f55c19f24d7744364495541b685539a4ee
  */
 public class GetFileAtCommitID extends AbstractAPIHandler implements APIHandler {
     private static final long serialVersionUID = -5686523277755750923L;
@@ -31,7 +33,7 @@ public class GetFileAtCommitID extends AbstractAPIHandler implements APIHandler 
 
     @Override
     public String getAPIPath() {
-        return "/cms/GetFileAtCommitID.json";
+        return "/cms/getFileAtCommitID.json";
     }
 
     @Override
@@ -48,8 +50,6 @@ public class GetFileAtCommitID extends AbstractAPIHandler implements APIHandler 
     public ServiceResponse serviceImpl(Query call, HttpServletResponse response, Authorization rights, final JsonObjectWithDefault permissions) throws APIException {
         JSONObject json = new JSONObject(true);
         json.put("accepted", false);
-        JSONArray commitsArray;
-        commitsArray = new JSONArray();
         String model_name = call.get("model", "general");
         File model = new File(DAO.model_watch_dir, model_name);
         String group_name = call.get("group", "Knowledge");
@@ -58,6 +58,16 @@ public class GetFileAtCommitID extends AbstractAPIHandler implements APIHandler 
         File language = new File(group, language_name);
         String skill_name = call.get("skill", "wikipedia");
         File skill = new File(language, skill_name + ".txt");
+
+        String commitID  = call.get("commitID", null);
+
+        if(commitID==null){
+            JSONObject error = new JSONObject();
+            error.put("message","Commit ID is Null");
+            error.put("accepted", false);
+            return new ServiceResponse(error);
+
+        }
         String path = skill.getPath().replace(DAO.model_watch_dir.toString(), "models");
 
         //Add to git
@@ -69,11 +79,11 @@ public class GetFileAtCommitID extends AbstractAPIHandler implements APIHandler 
                     .findGitDir() // scan up the file system tree
                     .build();
 
-            ObjectId lastCommitId = repository.resolve(Constants.HEAD);
+            ObjectId CommitIdObject = repository.resolve(commitID);
 
             // a RevWalk allows to walk over commits based on some filtering that is defined
             try (RevWalk revWalk = new RevWalk(repository)) {
-                RevCommit commit = revWalk.parseCommit(lastCommitId);
+                RevCommit commit = revWalk.parseCommit(CommitIdObject);
                 // and using commit's tree find the path
                 RevTree tree = commit.getTree();
                 System.out.println("Having tree: " + tree);
@@ -91,7 +101,20 @@ public class GetFileAtCommitID extends AbstractAPIHandler implements APIHandler 
                     ObjectLoader loader = repository.open(objectId);
 
                     // and then one can the loader to read the file
-                    loader.copyTo(System.out);
+                    OutputStream output = new OutputStream(){
+                        private StringBuilder string = new StringBuilder();
+                        @Override
+                        public void write(int b) throws IOException {
+                            this.string.append((char) b );
+                        }
+
+                        //Netbeans IDE automatically overrides this toString()
+                        public String toString(){
+                            return this.string.toString();
+                        }
+                    };
+                    loader.copyTo(output);
+                    json.put("file",output);
                 }
 
                 revWalk.dispose();
