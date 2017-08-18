fdb077df28beacdc8765078d810515ea39346235
DravitLochan <dlochangupta@gmail.com>
2017-08-18 14:42:03 +0530

    lexicographical sort

--- src/ai/susi/server/api/cms/ListSkillService.java
+++ src/ai/susi/server/api/cms/ListSkillService.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Map;
 
 /**
@@ -35,7 +36,7 @@ public class ListSkillService extends AbstractAPIHandler implements APIHandler {
     }
 
     @Override
-    public ServiceResponse serviceImpl(Query call, HttpServletResponse response, Authorization rights, final JsonObjectWithDefault permissions) {
+    public ServiceResponse serviceImpl(Query call, HttpServletResponse response, Authorization rights, final JsonObjectWithDefault permissions) throws APIException {
 
         String model_name = call.get("model", "general");
         File model = new File(DAO.model_watch_dir, model_name);
@@ -49,57 +50,83 @@ public class ListSkillService extends AbstractAPIHandler implements APIHandler {
         ArrayList<String> fileList = new ArrayList<String>();
         fileList =  listFilesForFolder(language, fileList);
         JsonTray skillRating = DAO.skillRating;
-            for (String skill : fileList) {
-                JSONObject skillMetadata = new JSONObject();
-               skill = skill.replace(".txt", "");
-                skillMetadata.put("developer_privacy_policy", JSONObject.NULL);
-                skillMetadata.put("descriptions",JSONObject.NULL);
-                skillMetadata.put("image", JSONObject.NULL);
-                skillMetadata.put("author", JSONObject.NULL);
-                skillMetadata.put("author_url", JSONObject.NULL);
-                skillMetadata.put("skill_name", JSONObject.NULL);
-                skillMetadata.put("terms_of_use", JSONObject.NULL);
-                skillMetadata.put("dynamic_content", false);
-                skillMetadata.put("examples", JSONObject.NULL);
-                skillMetadata.put("skill_rating", JSONObject.NULL);
-                for (Map.Entry<String, SusiSkill> entry : DAO.susi.getSkillMetadata().entrySet()) {
-                    String path = entry.getKey();
-                    if ((path.indexOf("/" + model_name + "/") > 0)
-                            && (path.indexOf("/" + group_name + "/") > 0) &&
-                            (path.indexOf("/" + language_name + "/") > 0) &&
-                            (path.indexOf("/" + skill + ".txt") > 0)) {
-                        skillMetadata.put("developer_privacy_policy", entry.getValue().getDeveloperPrivacyPolicy() ==null ? JSONObject.NULL:entry.getValue().getDeveloperPrivacyPolicy());
-                        skillMetadata.put("descriptions", entry.getValue().getDescription() ==null ? JSONObject.NULL:entry.getValue().getDescription());
-                        skillMetadata.put("image", entry.getValue().getImage() ==null ? JSONObject.NULL: entry.getValue().getImage());
-                        skillMetadata.put("author", entry.getValue().getAuthor()  ==null ? JSONObject.NULL:entry.getValue().getAuthor());
-                        skillMetadata.put("author_url", entry.getValue().getAuthorURL() ==null ? JSONObject.NULL:entry.getValue().getAuthorURL());
-                        skillMetadata.put("skill_name", entry.getValue().getSkillName() ==null ? JSONObject.NULL: entry.getValue().getSkillName());
-                        skillMetadata.put("terms_of_use", entry.getValue().getTermsOfUse() ==null ? JSONObject.NULL:entry.getValue().getTermsOfUse());
-                        skillMetadata.put("dynamic_content", entry.getValue().getDynamicContent());
-                        skillMetadata.put("examples", entry.getValue().getExamples() ==null ? JSONObject.NULL: entry.getValue().getExamples());
-                    }
-                }
+        if(call.get("applyFilter", false)) {
+            String filter_name = call.get("filter_name", null);
+            String filter_type = call.get("filter_type", null);
 
-                if (skillRating.has(model_name)) {
-                    JSONObject modelName = skillRating.getJSONObject(model_name);
-                    if (modelName.has(group_name)) {
-                        JSONObject groupName = modelName.getJSONObject(group_name);
-                        if (groupName.has(language_name)) {
-                            JSONObject  languageName = groupName.getJSONObject(language_name);
-                            if (languageName.has(skill)) {
-                                JSONObject skillName = languageName.getJSONObject(skill);
-                               skillMetadata.put("skill_rating", skillName);
-                            }
+            // Check for empty or null filter
+            if(filter_name.trim() == null || filter_type.trim() == null) {
+                throw new APIException(422,"Bad Filters.");
+            }
+            filter_name = filter_name.toLowerCase();
+            filter_type = filter_type.toLowerCase();
+
+            if(filter_type.equals("date")) {
+                if(filter_name.equals("ascending")) {
+
+                } else {
+
+                }
+            } else if(filter_type.equals("lexicographical")) {
+                if(filter_name.equals("ascending")) {
+                    fileList.sort((s, t1) -> s.compareToIgnoreCase(t1));
+                } else {
+                    fileList.sort((s, t1) -> t1.compareToIgnoreCase(s));
+                }
+            }
+
+        }
+        for (String skill : fileList) {
+            JSONObject skillMetadata = new JSONObject();
+            skill = skill.replace(".txt", "");
+            skillMetadata.put("developer_privacy_policy", JSONObject.NULL);
+            skillMetadata.put("descriptions",JSONObject.NULL);
+            skillMetadata.put("image", JSONObject.NULL);
+            skillMetadata.put("author", JSONObject.NULL);
+            skillMetadata.put("author_url", JSONObject.NULL);
+            skillMetadata.put("skill_name", JSONObject.NULL);
+            skillMetadata.put("terms_of_use", JSONObject.NULL);
+            skillMetadata.put("dynamic_content", false);
+            skillMetadata.put("examples", JSONObject.NULL);
+            skillMetadata.put("skill_rating", JSONObject.NULL);
+            for (Map.Entry<String, SusiSkill> entry : DAO.susi.getSkillMetadata().entrySet()) {
+                String path = entry.getKey();
+                if ((path.indexOf("/" + model_name + "/") > 0)
+                        && (path.indexOf("/" + group_name + "/") > 0) &&
+                        (path.indexOf("/" + language_name + "/") > 0) &&
+                        (path.indexOf("/" + skill + ".txt") > 0)) {
+                    skillMetadata.put("developer_privacy_policy", entry.getValue().getDeveloperPrivacyPolicy() ==null ? JSONObject.NULL:entry.getValue().getDeveloperPrivacyPolicy());
+                    skillMetadata.put("descriptions", entry.getValue().getDescription() ==null ? JSONObject.NULL:entry.getValue().getDescription());
+                    skillMetadata.put("image", entry.getValue().getImage() ==null ? JSONObject.NULL: entry.getValue().getImage());
+                    skillMetadata.put("author", entry.getValue().getAuthor()  ==null ? JSONObject.NULL:entry.getValue().getAuthor());
+                    skillMetadata.put("author_url", entry.getValue().getAuthorURL() ==null ? JSONObject.NULL:entry.getValue().getAuthorURL());
+                    skillMetadata.put("skill_name", entry.getValue().getSkillName() ==null ? JSONObject.NULL: entry.getValue().getSkillName());
+                    skillMetadata.put("terms_of_use", entry.getValue().getTermsOfUse() ==null ? JSONObject.NULL:entry.getValue().getTermsOfUse());
+                    skillMetadata.put("dynamic_content", entry.getValue().getDynamicContent());
+                    skillMetadata.put("examples", entry.getValue().getExamples() ==null ? JSONObject.NULL: entry.getValue().getExamples());
+                }
+            }
+
+            if (skillRating.has(model_name)) {
+                JSONObject modelName = skillRating.getJSONObject(model_name);
+                if (modelName.has(group_name)) {
+                    JSONObject groupName = modelName.getJSONObject(group_name);
+                    if (groupName.has(language_name)) {
+                        JSONObject  languageName = groupName.getJSONObject(language_name);
+                        if (languageName.has(skill)) {
+                            JSONObject skillName = languageName.getJSONObject(skill);
+                            skillMetadata.put("skill_rating", skillName);
                         }
                     }
                 }
-
-               skillObject.put(skill, skillMetadata);
             }
-                json.put("model", model_name)
+
+            skillObject.put(skill, skillMetadata);
+        }
+        json.put("model", model_name)
                 .put("group", group_name)
                 .put("language", language_name)
-                        .put("skills", skillObject);
+                .put("skills", skillObject);
         json.put("accepted", true);
         json.put("message","Success: Fetched skill list");
         return new ServiceResponse(json);
