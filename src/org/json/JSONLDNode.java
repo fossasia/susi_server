86cfc52de81312d14ec16b1e1d56abd1cba5362a
Michael Peter Christen <mc@yacy.net>
2018-06-04 17:59:06 +0200

    JSON-LD: first stub

---
+++ src/org/json/JSONLDNode.java
@@ -0,0 +1,48 @@
+/**
+ *  JSONLDNode
+ *  Copyright 04.06.2018 by Michael Peter Christen, @0rb1t3r
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program in the file lgpl21.txt
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.json;
+
+public class JSONLDNode extends JSONObject {
+
+    public static final String BASE = "@base";
+    public static final String BLANK_NODE_PREFIX = "_:";
+    public static final String CONTAINER = "@container";
+    public static final String CONTEXT = "@context";
+    public static final String DEFAULT = "@default";
+    public static final String EMBED = "@embed";
+    public static final String EMBED_CHILDREN = "@embedChildren";
+    public static final String EXPLICIT = "@explicit";
+    public static final String GRAPH = "@graph";
+    public static final String ID = "@id";
+    public static final String INDEX = "@index";
+    public static final String LANGUAGE = "@language";
+    public static final String LIST = "@list";
+    public static final String NONE = "@none";
+    public static final String NULL = "@null";
+    public static final String OMIT_DEFAULT = "@omitDefault";
+    public static final String PRESERVE = "@preserve";
+    public static final String REQUIRE_ALL = "@requireAll";
+    public static final String REVERSE = "@reverse";
+    public static final String SET = "@set";
+    public static final String TYPE = "@type";
+    public static final String VALUE = "@value";
+    public static final String VOCAB = "@vocab";
+	
+}
