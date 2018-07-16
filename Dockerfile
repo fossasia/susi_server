fossasia/susi_server

commit 41ad6029cdfbb6f2e2c791ea59f8b27b3965554d

The Docker images were written very poorly (#951)
Docker layers are immutable -- so you need to clean up in the same layer

Dockerfile
@@ -1,32 +1,27 @@
-FROM ubuntu:latest
-LABEL maintainer="ansgar.schmidt@gmx.net"
+FROM openjdk:8
+LABEL maintainer="Ansgar Schmidt <ansgar.schmidt@gmx.net>"
 ENV DEBIAN_FRONTEND noninteractive
+CMD ["bin/start.sh", "-Idn"]
+# Expose the web interface ports
+EXPOSE 80 443
 
-# update
-RUN apt-get update
-RUN apt-get upgrade -y
+RUN apt-get update && \
+apt-get upgrade -y &&
+apt-get install -y git
+&& rm -rf /var/lib/apt/lists/*
 
-# add packages
-RUN apt-get install -y git openjdk-8-jdk
+
 
 # clone the github repo
-RUN git clone --recursive https://github.com/fossasia/susi_server.git
+RUN git clone --recursive https://github.com/fossasia/susi_server.git && \
+git submodule update --init --recursive
 WORKDIR susi_server
-RUN git submodule update --init --recursive
 
 # compile
 RUN ./gradlew assemble
 
-# Expose the web interface ports
-EXPOSE 80 443
-
 # change config file
-RUN sed -i.bak 's/^\(port.http=\).*/\180/'                conf/config.properties
-RUN sed -i.bak 's/^\(port.https=\).*/\1443/'              conf/config.properties
-RUN sed -i.bak 's/^\(upgradeInterval=\).*/\186400000000/' conf/config.properties
-
-# hack until loklak support no-daemon
-RUN echo "while true; do sleep 10;done" >> bin/start.sh
-
-# start loklak
-CMD ["bin/start.sh", "-Idn"]
+RUN sed -i.bak 's/^\(port.http=\).*/\180/'            conf/config.properties && \
+sed -i.bak 's/^\(port.https=\).*/\1443/'              conf/config.properties && \
+sed -i.bak 's/^\(upgradeInterval=\).*/\186400000000/' conf/config.properties && \
+echo "while true; do sleep 10;done" >> bin/start.sh

kubernetes/images/Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:latest
+FROM openjdk:8-alpine
 LABEL maintainer="Ansgar Schmidt <ansgar.schmidt@gmx.net>"
 
 ARG BRANCH
@@ -7,7 +7,7 @@ ARG COMMIT_HASH
 ENV COMMIT_HASH ${COMMIT_HASH:-null}
 ENV BRANCH ${BRANCH:-development}
 # Install packages and clone from Github
-RUN apk update && apk add git openjdk8 bash && \
+RUN apk update && apk add --no-cache git bash && \
     cd / && git clone https://github.com/fossasia/susi_skill_data.git && \
     git clone https://github.com/fossasia/susi_server.git /susi_server
 WORKDIR /susi_server

commit 497688b91e4cf49e64b1c80aa6a9c1de8427cde0

fixes regression introduced in #951

Dockerfile
@@ -6,9 +6,9 @@ CMD ["bin/start.sh", "-Idn"]
 EXPOSE 80 443
 
 RUN apt-get update && \
-apt-get upgrade -y &&
-apt-get install -y git
-&& rm -rf /var/lib/apt/lists/*
+apt-get upgrade -y && \
+apt-get install -y git && \
+rm -rf /var/lib/apt/lists/*
 
 
 