253256759e9b26b45862483fc8a37ad62e5b2ad1
PRP <prp9812@gmail.com>
2018-07-27 22:43:18 +0530

    Implement API for storing API keys

--- src/ai/susi/SusiServer.java
+++ src/ai/susi/SusiServer.java
@@ -455,6 +455,7 @@ public class SusiServer {
                 // aaa services
                 StatusService.class,
                 AppsService.class,
+                ApiKeysService.class, 
                 AuthorizationDemoService.class,
                 LoginService.class,
                 PasswordRecoveryService.class,
