05215f1fae717fe13a608e0b8dd104bd0a2dea17
yashasvi128 <yashasvi1981.cse16@chitkara.edu.in>
2018-12-05 20:11:31 +0530

    DOCS: Added swagger annotations for RemoveUserDevices, ShowAdminService and UserAccountPermissions

--- src/ai/susi/server/api/aaa/ShowAdminService.java
+++ src/ai/susi/server/api/aaa/ShowAdminService.java
@@ -2,19 +2,29 @@ package ai.susi.server.api.aaa;
 
 import ai.susi.json.JsonObjectWithDefault;
 import ai.susi.server.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by dravit on 10/8/17.
  * To show or not to show admin option to the user who is logged in,
  * this servlet will return a boolean flag on attribute showAdmin
  */
+@Path("/aaa/showAdminService.json")
+@Produces(MediaType.APPLICATION_JSON)
+@Api(value = "ShowAdminService",
+        description = "This Endpoint returns the flag of a user account to show or not to show admin option.")
 public class ShowAdminService extends AbstractAPIHandler implements APIHandler{
-    /**
-     * 
-     */
+
     private static final long serialVersionUID = 8597701313383109201L;
 
     @Override
@@ -27,6 +37,11 @@ public class ShowAdminService extends AbstractAPIHandler implements APIHandler{
         return UserRole.ANONYMOUS;
     }
 
+    @GET
+    @ApiOperation(httpMethod = "GET", value = "Resource  To show or not to show admin option to the user who is logged in")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,
+                    message = "showAdmin : true") })
     @Override
     public JSONObject getDefaultPermissions(UserRole baseUserRole) {
         return null;
