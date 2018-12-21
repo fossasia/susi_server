3d4e44bcd6a0209a6e7a1aeb424d0110bb3f7287
Michael Christen <mc@yacy.net>
2018-12-21 02:40:42 +0100

    copy instead of linking of latest release

--- bin/release.sh
+++ bin/release.sh
@@ -28,11 +28,13 @@ done
 GITHASH=`git log -n 1 --pretty=format:"%h"`
 RELEASE_PATH=release
 RELEASE_FILE=susi_server_binary_`date "+%Y%m%d"`_$GITHASH
-LATEST_FILE=susi_server_binary_latest.tar.gz
+LATEST_FILE=susi_server_binary_latest
 
 # clean up
 rm -Rf $RELEASE_PATH/$RELEASE_FILE
 rm -f $RELEASE_PATH/${RELEASE_FILE}.tar.gz
+rm -Rf $RELEASE_PATH/$LATEST_FILE
+rm -f $RELEASE_PATH/${LATEST_FILE}.tar.gz
 
 # make release file structure
 mkdir $RELEASE_PATH/$RELEASE_FILE
@@ -46,16 +48,18 @@ cp -R html $RELEASE_PATH/$RELEASE_FILE/
 cp bin/*.sh $RELEASE_PATH/$RELEASE_FILE/bin/
 cp $JARFILE $RELEASE_PATH/$RELEASE_FILE/$JARFILE
 
+# make a complete copy
+cp -R $RELEASE_PATH/$RELEASE_FILE $RELEASE_PATH/$LATEST_FILE
+
 # compress folder
 cd $RELEASE_PATH
 tar cf ${RELEASE_FILE}.tar $RELEASE_FILE
+tar cf ${LATEST_FILE}.tar $LATEST_FILE
 gzip -9 ${RELEASE_FILE}.tar
+gzip -9 ${LATEST_FILE}.tar
 rm -Rf $RELEASE_FILE
+rm -Rf $LATEST_FILE
 cd ..
-
-# link latest file
-rm -f $RELEASE_PATH/$LATEST_FILE
-ln -s $RELEASE_PATH/${RELEASE_FILE}.tar.gz $RELEASE_PATH/$LATEST_FILE
 
 # finish!
 echo "wrote ${RELEASE_FILE}.tar.gz"
