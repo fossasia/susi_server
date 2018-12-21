7d03bfde936e41cc6a3f28682ed54ea9683e4f24
Michael Christen <mc@yacy.net>
2018-12-21 02:16:24 +0100

    finished binary release build script

--- bin/release.sh
+++ bin/release.sh
@@ -1,17 +1,56 @@
-#!/usr/bin/env sh
-cd "`dirname $0`"
+#!/usr/bin/env bash
+
+cd $(dirname $0)/..
+source bin/preload.sh
+
+while getopts ":pb" opt; do
+    case $opt in
+        p)
+	    git reset --hard
+            git pull origin master
+            ;;
+        b)
+	    gradle assemble
+            ;;
+        \?)
+            echo "Usage: $0 [options...]"
+            echo -e " -p\tpull latest commit and overwrite all local changes"
+            echo -e " -b\trun the gradle build script"
+            exit 1
+            ;;
+    esac
+done
+
+# variables used from preload.sh
+# JARFILE="build/libs/susi_server-all.jar"
+
+# make release file name
+GITHASH=`git log -n 1 --pretty=format:"%h"`
+RELEASE_PATH=release
+RELEASE_FILE=susi_server_binary_`date "+%Y%m%d"`_$GITHASH
+
+# clean up
+rm -Rf $RELEASE_PATH/$RELEASE_FILE
+rm -f $RELEASE_PATH/${RELEASE_FILE}.tar.gz
+
+# make release file structure
+mkdir $RELEASE_PATH/$RELEASE_FILE
+mkdir $RELEASE_PATH/$RELEASE_FILE/bin
+mkdir $RELEASE_PATH/$RELEASE_FILE/build
+mkdir $RELEASE_PATH/$RELEASE_FILE/build/libs
+
+# copy files
+cp -R conf $RELEASE_PATH/$RELEASE_FILE/
+cp -R html $RELEASE_PATH/$RELEASE_FILE/
+cp bin/*.sh $RELEASE_PATH/$RELEASE_FILE/bin/
+cp $JARFILE $RELEASE_PATH/$RELEASE_FILE/$JARFILE
+
+# compress folder
+cd $RELEASE_PATH
+tar cf ${RELEASE_FILE}.tar $RELEASE_FILE
+gzip -9 ${RELEASE_FILE}.tar
+rm -Rf $RELEASE_FILE
 cd ..
 
-githash=`git log -n 1 --pretty=format:"%h"`
-release_path=release
-release=susi_server_`date "+%Y%m%d"`_$githash
-jar=build/libs/susi_server-all.jar
-rm -Rf $release_path/$release
-mkdir $release_path/$release
-cp -R conf $release_path/$release/
-cp -R html $release_path/$release/
-mkdir $release_path/$release/bin
-cp bin/*.sh $release_path/$release/bin/
-cp $jar $release_path/$release/
-#gradle assemble
-
+# finish!
+echo "wrote ${RELEASE_FILE}.tar.gz"
