fb2de9b86ac8e648b32b22df6518d79e7a3f1987
Michael Peter Christen <mc@yacy.net>
2019-05-26 00:15:34 +0200

    open browser window only on mac oder windows

--- src/ai/susi/SusiServer.java
+++ src/ai/susi/SusiServer.java
@@ -234,7 +234,7 @@ public class SusiServer {
         DAO.log("Start time until server is up: " + (System.currentTimeMillis() - starttime) + " milliseconds");
 
         // if this is not headless, we can open a browser automatically
-        OS.openBrowser("http://127.0.0.1:" + httpPort);
+        if (OS.isMacArchitecture || OS.isWin32 || OS.isWindows) OS.openBrowser("http://127.0.0.1:" + httpPort);
 
         DAO.log("Finished startup!");
 
