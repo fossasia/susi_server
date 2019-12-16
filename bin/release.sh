48fae2512d3ddcd4f1ffbd0d73ea52b40cc5e4a9
Norbert Preining <norbert@preining.info>
2019-12-16 16:07:42 +0900

    fix release packaging of system integration stuff

--- bin/release.sh
+++ bin/release.sh
@@ -58,8 +58,8 @@ cp -R conf $RELEASE_PATH/$RELEASE_FILE/
 cp -R html $RELEASE_PATH/$RELEASE_FILE/
 cp bin/*.sh $RELEASE_PATH/$RELEASE_FILE/bin/
 cp $JARFILE $RELEASE_PATH/$RELEASE_FILE/$JARFILE
-cp systemd/* $RELEASE_PATH/$RELEASE_FILE/system-integration/systemd/
-cp desktop/* $RELEASE_PATH/$RELEASE_FILE/system-integration/desktop/
+cp system-integration/systemd/* $RELEASE_PATH/$RELEASE_FILE/system-integration/systemd/
+cp system-integration/desktop/* $RELEASE_PATH/$RELEASE_FILE/system-integration/desktop/
 
 
 # make a complete copy
