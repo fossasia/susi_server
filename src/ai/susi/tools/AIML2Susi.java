2f1e25adabcba659abcad765903896ef6ab39689
Michael Peter Christen <mc@yacy.net>
2020-03-02 22:39:55 +0100

    progress with aiml parser

--- src/ai/susi/tools/AIML2Susi.java
+++ src/ai/susi/tools/AIML2Susi.java
@@ -42,7 +42,6 @@ import ai.susi.mind.SusiLanguage;
 
 public class AIML2Susi {
 
-    
     public static JSONObject readAIMLSkill(File file) throws Exception {
         // read the file as string
         BufferedReader br = new BufferedReader(new FileReader(file));
@@ -50,7 +49,7 @@ public class AIML2Susi {
         StringBuilder buf=new StringBuilder();
         while ((str = br.readLine()) != null) buf.append(str);
         br.close();
-        
+
         // parse the string as xml into a node object
         InputStream is = new ByteArrayInputStream(buf.toString().getBytes(StandardCharsets.UTF_8));
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -58,8 +57,8 @@ public class AIML2Susi {
         Document doc = dBuilder.parse(is);
         doc.getDocumentElement().normalize();
 
-        System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-        
+        //System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
         Node root = doc.getDocumentElement();
         NodeList nl = root.getChildNodes();
         JSONObject json = new JSONObject();
@@ -71,64 +70,123 @@ public class AIML2Susi {
             if (nodename.equals("category")) {
                 JSONObject intent = readAIMLCategory(node);
                 if (intent != null && intent.length() > 0) intents.put(intent);
+            } else if (nodename.equals("#comment")) {
+            } else if (nodename.equals("#text")) {
+            } else if (nodename.equals("topic")) {
+            } else {
+                System.out.println("unknown aiml nodename: " + nodename); // hack until this disappears
             }
             //System.out.println("ROOT NODE " + nl.item(i).getNodeName());
         }
         return json;
     }
-    
+
     public static JSONObject readAIMLCategory(Node category) {
         NodeList nl = category.getChildNodes();
-        String[] phrases = null;
-        String[] answers = null;
+        List<String> phrases = null;
+        List<String> answers = null;
         for (int i = 0; i < nl.getLength(); i++) {
             Node node = nl.item(i);
             String nodename = node.getNodeName().toLowerCase();
             //System.out.println("CATEGORYY NODE " + node.getNodeName());
             if (nodename.equals("pattern")) {
-                phrases = readAIMLSentences(node);
-            } else if (nodename.equals("that")) {
-                
+                phrases = readAIMLPattern(node.getChildNodes());
             } else if (nodename.equals("template")) {
-                answers = readAIMLSentences(node);
+                answers = readAIMLTemplate(node.getChildNodes(), true);
+            } else if (nodename.equals("that")) {
+            } else if (nodename.equals("#text")) {
+            } else if (nodename.equals("category")) {
+            } else {
+                System.out.println("unknown category nodename: " + nodename); // hack until this disappears
             }
         }
         if (phrases != null && answers != null) {
-            return SusiIntent.answerIntent(phrases, null, answers, false, 0, null, null, null, null, SusiLanguage.unknown);
+            return SusiIntent.answerIntent(phrases.toArray(new String[phrases.size()]), null, answers.toArray(new String[answers.size()]), false, 0, null, null, null, null, SusiLanguage.unknown);
         }
         return null;
     }
-    
-    public static String[] readAIMLSentences(Node pot) {
-        NodeList nl = pot.getChildNodes();
+
+    public static List<String> readAIMLPattern(NodeList nl) {
         List<String> sentences = new ArrayList<>();
         for (int i = 0; i < nl.getLength(); i++) {
             Node node = nl.item(i);
             String nodename = node.getNodeName().toLowerCase();
-            //System.out.println("SENTENCE NODE " + node.getNodeName());
-            if (nodename.equals("pattern")) {
-                sentences.add(node.getTextContent());
-            } else if (nodename.equals("that")) {
-                
-            } else if (nodename.equals("template")) {
-                
+            String nodevalue = node.getNodeValue();
+            if (nodename.equals("#text")) {
+                sentences.add(nodevalue);
+            } else if (nodename.equals("bot")) {
+            } else {
+                System.out.println("unknown pattern nodename: " + nodename); // hack until this disappears
             }
         }
-        return sentences.toArray(new String[sentences.size()]);
+        return sentences;
     }
-    
+
+    public static List<String> readAIMLTemplate(NodeList nl, boolean visible) {
+        List<String> sentences = new ArrayList<>();
+        for (int i = 0; i < nl.getLength(); i++) {
+            Node node = nl.item(i);
+            String nodename = node.getNodeName().toLowerCase();
+            String nodevalue = node.getNodeValue();
+
+            // see https://www.pandorabots.com/docs/aiml-reference/ as reference
+            // the following conditions are listed in reverse order of occurrences
+            if (nodename.equals("#text")) {
+                if (nodevalue == null) nodevalue = "";
+                nodevalue = nodevalue.trim();
+                if (nodevalue.length() > 0) sentences.add(nodevalue);
+            } else if (nodename.equals("srai")) {
+                List<String> srai = readAIMLTemplate(node.getChildNodes(), visible);
+                if (srai.size() != 1) throw new RuntimeException("unexpected number of srai nodes: " + srai.size());
+                sentences.add("`" + srai.get(0) + "`");
+            } else if (nodename.equals("think")) {
+                System.out.println("unknown template node function: " + nodename);
+                List<String> invisible = readAIMLTemplate(node.getChildNodes(), false);
+                sentences.addAll(invisible);
+            } else if (nodename.equals("set")) {
+            } else if (nodename.equals("random")) {
+            } else if (nodename.equals("person")) {
+            } else if (nodename.equals("bot")) {
+            } else if (nodename.equals("get")) {
+            } else if (nodename.equals("br")) {
+            } else if (nodename.equals("#comment")) {
+            } else if (nodename.equals("formal")) {
+            } else if (nodename.equals("sr")) {
+            } else if (nodename.equals("that")) {
+            } else if (nodename.equals("em")) {
+            } else if (nodename.equals("star")) {
+            } else if (nodename.equals("a")) {
+            } else if (nodename.equals("date")) {
+            } else if (nodename.equals("condition")) {
+            } else if (nodename.equals("input")) {
+            } else if (nodename.equals("thatstar") || nodename.equals("thastar")) {
+            } else if (nodename.equals("p")) {
+            } else if (nodename.equals("id")) {
+            } else if (nodename.equals("size")) {
+            } else if (nodename.equals("img")) {
+            } else if (nodename.equals("uppercase")) {
+            } else if (nodename.equals("ul")) {
+            } else if (nodename.equals("get_likes")) {
+            } else if (nodename.equals("version")) {
+            } else {
+                System.out.println("unknown template nodename: " + nodename); // hack until this disappears
+            }
+        }
+        return sentences;
+    }
+
     public static void main(String[] args) {
-    	File archive = new File("/Users/admin/git/AIMLemotions");
-    	String[] list = archive.list();
-    	for (String f: list) {
-    		if (! f.endsWith(".aiml")) continue;
-    		try {
-				JSONObject j = readAIMLSkill(new File(archive, f));
-				System.out.println("AIML: " + f);
-				System.out.println(j.toString(2));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-    	}
+        File archive = new File("/Users/admin/git/AIMLemotions");
+        String[] list = archive.list();
+        for (String f: list) {
+            if (! f.endsWith(".aiml")) continue;
+            try {
+                JSONObject j = readAIMLSkill(new File(archive, f));
+                //System.out.println("AIML: " + f);
+                //System.out.println(j.toString(2));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
