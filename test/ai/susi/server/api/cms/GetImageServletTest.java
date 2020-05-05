0cab97de008abd369fec027d3411ecd8d0a82fb3
iamareebjamal <jamal.areeb@gmail.com>
2020-05-05 10:12:11 +0530

    test: Write SkillQueryParserTest

--- test/ai/susi/server/api/cms/GetImageServletTest.java
+++ test/ai/susi/server/api/cms/GetImageServletTest.java
@@ -9,14 +9,12 @@ import org.junit.Test;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -32,13 +30,9 @@ public class GetImageServletTest {
             .forEach(File::mkdirs);
     }
 
-    private StubServletOutputStream testImageAccess(Map<String, String> params) throws IOException {
-        HttpServletRequest request = mock(HttpServletRequest.class);
+    private StubServletOutputStream testImageAccess(String... params) throws IOException {
+        HttpServletRequest request = TestHelpers.getMockRequestWithParams(params);
         HttpServletResponse response = mock(HttpServletResponse.class);
-
-        when(request.getServletPath()).thenReturn("test");
-        when(request.getRemoteHost()).thenReturn("test");
-        params.forEach((key, value) -> when(request.getParameter(key)).thenReturn(value));
 
         StubServletOutputStream stream = new StubServletOutputStream();
         when(response.getOutputStream()).thenReturn(stream);
@@ -52,68 +46,60 @@ public class GetImageServletTest {
         assertFalse("Servlet should not access data external to the base directory. Data: " + content, content.contains(data));
     }
 
-    private Map<String, String> getParams(String... args) {
-        Map<String, String> map = new HashMap<>();
-        for (int i = 0; i < args.length - 1; i+=2) {
-            map.put(args[i], args[i+1]);
-        }
-        return map;
-    }
-
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotAccessExtraDataForImageParam() throws IOException {
-        StubServletOutputStream stream = testImageAccess(getParams("image", "../settings/private.settings.json"));
+        StubServletOutputStream stream = testImageAccess("image", "../settings/private.settings.json");
         assertNotContains(stream.getContent(), "private_key");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotAccessExtraDataForSliderImageParam() throws IOException {
-        StubServletOutputStream stream = testImageAccess(getParams("sliderImage", "../settings/private.settings.json"));
+        StubServletOutputStream stream = testImageAccess("sliderImage", "../settings/private.settings.json");
         assertNotContains(stream.getContent(), "private_key");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotAccessExtraDataForAvatarImageParam() throws IOException {
-        StubServletOutputStream stream = testImageAccess(getParams(
+        StubServletOutputStream stream = testImageAccess(
                 "image", "../../settings/private.settings.json",
                 "avatar", "true"
-        ));
+        );
         assertNotContains(stream.getContent(), "private_key");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotAccessExtraDataForModelImage() throws IOException {
-        testImageAccess(getParams(
+        testImageAccess(
                 "image", "../../settings/private.settings.json",
                 "model", "..",
                 "language", "..",
                 "group", ".."
-        ));
+        );
     }
 
     @Test
     public void shouldAccessDefaultImageForAbsentModelImage() throws IOException {
-        StubServletOutputStream stream = testImageAccess(getParams(
+        StubServletOutputStream stream = testImageAccess(
                 "image", "images/test.jpg",
                 "model", "general",
                 "language", "en",
                 "group", "Knowledge"
-        ));
+        );
         assertEquals(stream.getContent(), IO.readFile(GetImageServlet.getDefaultImage()).toString());
     }
 
     @Test
     public void shouldAccessDefaultImageForAbsentAvatar() throws IOException {
-        StubServletOutputStream stream = testImageAccess(getParams(
+        StubServletOutputStream stream = testImageAccess(
                 "image", "avatar/haha.jpg",
                 "avatar", "true"
-        ));
+        );
         assertEquals(stream.getContent(), IO.readFile(GetImageServlet.getDefaultImage()).toString());
     }
 
     @Test
     public void shouldAccessDefaultImage() throws IOException {
-        StubServletOutputStream stream = testImageAccess(getParams());
+        StubServletOutputStream stream = testImageAccess();
         assertEquals(stream.getContent(), IO.readFile(GetImageServlet.getDefaultImage()).toString());
     }
 }
--- test/ai/susi/utils/TestHelpers.java
+++ test/ai/susi/utils/TestHelpers.java
@@ -3,10 +3,14 @@ package ai.susi.utils;
 import ai.susi.DAO;
 import ai.susi.SusiServer;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TestHelpers {
 
@@ -14,6 +18,31 @@ public class TestHelpers {
         Path data = FileSystems.getDefault().getPath("data");
         Map<String, String> config = SusiServer.readConfig(data);
         DAO.init(config, data, false);
+    }
+
+
+    public static HttpServletRequest getMockRequestWithParams(Map<String, String> params) {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+
+        // Required for last visit query in RemoteAccess.java
+        when(request.getServletPath()).thenReturn("test");
+        when(request.getRemoteHost()).thenReturn("test");
+
+        params.forEach((key, value) -> when(request.getParameter(key)).thenReturn(value));
+
+        return request;
+    }
+
+    public static HttpServletRequest getMockRequestWithParams(String... args) {
+        return getMockRequestWithParams(getParams(args));
+    }
+
+    private static Map<String, String> getParams(String... args) {
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < args.length - 1; i+=2) {
+            map.put(args[i], args[i+1]);
+        }
+        return map;
     }
 
 }
